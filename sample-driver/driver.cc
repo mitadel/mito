043612f08ace04179890a89cebb89a6769b92643
Bianca Giovanardi <biancagi@mit.edu>
2020-10-26 11:09:23 +0100

    sample-driver: Updated draft with Michael comments

--- sample-driver/driver.cc
+++ sample-driver/driver.cc
@@ -1,27 +1,27 @@
 #include <cmath>
-#include <summit/mesh/mesh.h>
-#include <summit/materials/material_library.h>
-#include <summit/materials/elastic/elastic.h>
-#include <summit/fem/function_space.h>
-#include <summit/systems/statics_system.h>
-#include <summit/solvers/linear_solver.h>
+#include <mito/mesh/mesh.h>
+#include <mito/materials/material_library.h>
+#include <mito/materials/elastic/elastic.h>
+#include <mito/fem/function_space.h>
+#include <mito/systems/statics_system.h>
+#include <mito/solvers/linear_solver.h>
 
-summit::vector<3> sourceTerm(const summit::vector<3>& x, summit::real t)
+mito::vector<3> sourceTerm(const mito::vector<3>& x, mito::real t)
 {
     return {x[0]*t, x[1], x[2]}; 
 }
 
-summit::vector<3> dirichletBC(const summit::vector<3>& x, summit::real t)
+mito::vector<3> dirichletBC(const mito::vector<3>& x, mito::real t)
 {
     return {0.0, 0.0, 0.0}; 
 }
 
-summit::vector<3> neumannBC(const summit::vector<3>& x, summit::real t)
+mito::vector<3> neumannBC(const mito::vector<3>& x, mito::real t)
 {
     return {-1.0, 0.0, 0.0}; 
 }
 
-bool filterLeft(const summit::vector<3>& x)
+bool filterLeft(const mito::vector<3>& x)
 {
     if (std::fabs(x[0]) < 1e-8)
         return true;
@@ -34,7 +34,7 @@ int main (int argc, char ** argv)
     // ------------------------------------
     // input file
     // ------------------------------------
-    summit::InputFile input("input.dat");
+    mito::InputFile input("input.dat");
     // print all parameters in input file
     input.Display();
     // material density 
@@ -50,27 +50,29 @@ int main (int argc, char ** argv)
     // Remark 2: the mesh file also contains the boundary 'physical' entities
     // ------------------------------------
     // instantiate a mesh object
-    summit::Mesh mesh("mesh.summit"); 
+    mito::Mesh mesh("mesh.mito"); 
 
     // ------------------------------------
     // material
     // Remark: would be nice to somehow define constitutive law from upper level
     // ------------------------------------
     // instantiate an elastic material
-    summit::Elastic elasticMaterial(rho, E, nu, 1 /*numIntVars*/, 1 /*plain strain*/);
+    mito::material::Elastic elasticMaterial(rho, E, nu, 1 /*numIntVars*/, 1 /*plain strain*/);
+    // or...
+    auto material = mito::material::elastic(rho, E, nu);
 
     /*
     stress, tangent, update     are function pointers
     parameters                  is the set of material parameters
     internalVariables           is the set of internal variables
-    summit::CustomMaterial myMaterial(&stress, &tangent, &update, parameters, internalVariables);
+    mito::CustomMaterial myMaterial(&stress, &tangent, &update, parameters, internalVariables);
     */
 
     // ------------------------------------
     // material library
     // ------------------------------------
     // instantiate a material library
-    summit::MaterialLibrary materialLibrary;
+    mito::MaterialLibrary materialLibrary();
     // add elastic material to the material library
     materialLibrary.AddMaterial(1, &elasticMaterial);
     // print all material properties in material library
@@ -83,37 +85,45 @@ int main (int argc, char ** argv)
     //         mesh to build different sets of shape functions...
     // ------------------------------------
     // instantiate a function space object
-    summit::FunctionSpace functionSpace(mesh, "CG", "P1");
+    mito::FunctionSpace functionSpace(mesh, "CG", "P1");
 
     // ------------------------------------
     // system
     // ------------------------------------
     // instantiate a system binding the functionSpace (math) and the materialLibrary (physics)
-    summit::System system(functionSpace, materialLibrary);
+    mito::System system(functionSpace, materialLibrary);
     system.AddSourceTerm(sourceTerm);
     system.AddDirichletBC("boundary A", dirichletBC);
     system.AddDirichletBC(filterLeft, dirichletBC);
     system.AddNeumannBC("boundary B", neumannBC);
 
+    // define the load to apply to the system (e.g. BCs and source terms)
+    mito::Load 
+    system.Add(load);
+
     // ------------------------------------
     // VTU writer
     // ------------------------------------
     // add fields to print
-    system.AddToVTU("displacement");
-    system.AddToVTU("boundary");
-    system.AddToVTU("forces");
-    system.AddToVTU("stress");
-    system.AddToVTU("analytic displacement", functorAnalytic);
+    auto vtu = vtuWriter("output");
+    vtu.Record("boundary") 
+    vtu.Record("forces") 
+    vtu.Record("stress") 
+    vtu.Record("analytic displacement", functorAnalytic); 
+    system.AddToWriter(vtu);
 
     // ------------------------------------
     // solver
     // ------------------------------------
     // instantiate a linear solver
-    summit::LinearSolver solver(&system);
-    // solve the problem (this assembles and solves the algebraic system of equations)
-    solver.Solve();
-    // write the solution to output file
-    system.PrintVTU("output", 0 /*step*/);
+    mito::LinearSolver solver;
+    solver.Add(system);
+
+    time loop {
+        // solve the problem (this assembles and solves the algebraic system of equations)
+        solver.Step(dt);
+        system.Write(t);
+    }
 
     // all done
     return 0;
