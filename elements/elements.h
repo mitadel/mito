4d6f37fd629aa230dbe64c14a858aab7666e57c6
Bianca Giovanardi <biancagi@mit.edu>
2020-11-24 19:19:28 +0100

    elements: Drafted classes ElementSet and ElementSetTri
    Co-authored-by: Anwar Koshakji <anwar.koshakji@gmail.com>

---
+++ elements/elements.h
@@ -0,0 +1,36 @@
+#ifndef __MITO__ELEMENTS__
+#define __MITO__ELEMENTS__
+
+#include "../mito.h"
+
+namespace mito {
+
+enum ElementType {TRI, TET};
+
+template<DIM D>
+class ElementSet
+{
+  public:
+
+    ElementSet(ElementType type) : _type(type) {}
+    ~ElementSet() {}
+
+    ElementType type() const {return _type;}
+    DIM dim() const {return D;};
+
+  private: 
+    ElementType _type;
+};
+
+class ElementSetTri : public ElementSet<DIM2>
+{
+  public:
+    ElementSetTri() : ElementSet<DIM2>(TRI) {}
+    ~ElementSetTri() {}
+
+};
+
+}
+#endif //__MITO__ELEMENTS__
+
+// end of file
