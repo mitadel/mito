0b3d9a0617de58e61a17747066dc5a3c34c2ee25
Bianca Giovanardi <biancagi@mit.edu>
2020-11-24 15:30:23 +0100

    math: Added class VectorField and Grad and Div operators

--- math/math.h
+++ math/math.h
@@ -2,22 +2,24 @@
 
 namespace mito {
 
-// f(X,x,t) with (X \in R^D1, x \in R^D2, t \in R) -> R
-template <DIM D1, DIM D2 = D1>
+// f(X,x,t) with (X \in R^D, x \in R^D2, t \in R) -> R
+template <DIM D, DIM D2 = D>
 class ScalarField {
 
-    typedef function<vector<D1>, real > function_t;
+    typedef function<vector<D>, real > function_t;
 
   public:
     // constructors
     ScalarField(function_t f) : _f(f), _Df() {}
-    ScalarField(function_t f, std::array<function_t, D1> Df) : 
+    ScalarField(function_t f, std::array<function_t, D> Df) : 
         _f(f), _Df(Df) {}
+    // TOFIX: This constructor should be deleted, but it is needed. See comment below. 
+    ScalarField() : _f(), _Df() {}
 
     ~ScalarField() {}
 
-    //inline real operator()(const vector<D1> & X, const vector<D2> & x, real t) const {
-    inline real operator()(const vector<D1> & X) const {
+    //inline real operator()(const vector<D> & X, const vector<D2> & x, real t) const {
+    inline real operator()(const vector<D> & X) const {
         // evaluate _f
         return _f(X);
     }
@@ -35,17 +37,80 @@ class ScalarField {
     // the function
     function_t _f;
     // the derivatives of f with respect to X (position in the reference configuration)
-    std::array<function_t, D1> _Df;
+    std::array<function_t, D> _Df;
 };
 
-// function to compute the Gradient of a scalar function at point X
-template<DIM D1>
-inline vector<D1> Grad(const ScalarField<D1> & function, const vector<D1> & X) {
-    vector<D1> GradX;
-    for (size_t i = 0; i < D1; ++i) {
+// template on vector dimension N, spatial dimension D
+template <size_t N, DIM D>
+class VectorField
+{
+  public:
+    VectorField(std::array<ScalarField<D>, N> components) : _components(components) {}
+    ~VectorField() {}
+
+    inline vector<N> operator()(const vector<D> & X) const {
+        vector<N> result;
+        for (int i = 0; i < N; ++i) {
+            result[i] = _components[i](X);
+        }
+        return std::move(result);
+    }
+
+    inline const ScalarField<D> & operator[](size_t i) const {
+        // assert there exists the i-th partial derivative
+        assert (i < _components.size());
+        // return the i-th component
+        return _components[i];
+    }
+
+    inline ScalarField<D> & operator[](size_t i) {
+        // assert there exists the i-th partial derivative
+        assert (i < _components.size());
+        // return the i-th component
+        return _components[i];
+    }
+
+  private:
+    std::array<ScalarField<D>, N> _components;
+};
+
+// function to compute the Divergence of a vector field at point X 
+template<DIM D>
+inline real Div(const VectorField<D, D> & function, const vector<D> & X) {
+    real DivX = 0.0;
+    for (size_t i(0); i < D; ++i) {
+        DivX += function[i].Df(i)(X);
+    }
+    return DivX;
+}
+
+// function to compute the Gradient of a scalar field at point X
+template<DIM D>
+inline vector<D> Grad(const ScalarField<D> & function, const vector<D> & X) {
+    vector<D> GradX;
+    for (size_t i = 0; i < D; ++i) {
         GradX[i] = function.Df(i)(X);
     }
-    return GradX;
+    return std::move(GradX);
+}
+
+// function to compute the Gradient of a vector field at point X
+template<DIM D>
+inline VectorField<D, D> Grad(const ScalarField<D> & function) {
+    // TOFIX: This is the reason why we need a simple constructor with no arguments...
+    //       We could avoid it if we could write something like:
+    //          std::array<ScalarField<D>, D> components{function.Df(0), ..., function.Df(D-1)};
+    // e.g.:
+    //std::array<ScalarField<D>, D> components {function.Df(0), function.Df(1)};
+    std::array<ScalarField<D>, D> components;
+    for (size_t i = 0; i < D; ++i) {
+        components[i] = function.Df(i);
+    }
+
+    VectorField<D, D> GradX(std::move(components));
+    return std::move(GradX);
 }
 
 }
+
+// end of file
--- math/test/main.cc
+++ math/test/main.cc
@@ -18,15 +18,31 @@ int main () {
     // its partial derivatives 
     std::array<mito::function<mito::vector<mito::DIM2>, mito::real>, mito::DIM2> Df = {Dx, Dy};
 
-    // instantiate a function object
+    // instantiate a scalar function object
     mito::ScalarField<mito::DIM2> cosine(f, Df);
 
     // a point in the reference configuration
     mito::vector<mito::DIM2> X = {0.0, 0.0};
 
     // evaluate f and its gradient
-    std::cout << cosine(X) << std::endl; 
-    std::cout << mito::Grad<mito::DIM2>(cosine, X) << std::endl; 
+    std::cout << "Evaluating cosine function cos(x[0] * x[1]) at X = " << X << " : " << 
+        cosine(X) << std::endl; 
+    std::cout << "Evaluating gradient of cosine function at X = " << X << " : " << 
+        mito::Grad<mito::DIM2>(cosine, X) << std::endl; 
+
+    // instantiate a vector function object
+    mito::VectorField<2, mito::DIM2> cosineVector({cosine, cosine});
+
+    // evaluate divergence
+    std::cout << "Evaluating divergence of cosine vector function at X = " << X << " : " << 
+        mito::Div<mito::DIM2>(cosineVector, X) << std::endl; 
+
+    // 
+    mito::VectorField<2, mito::DIM2> gradient = mito::Grad<mito::DIM2>(cosine);
+    std::cout << "Evaluating gradient of cosine function at X = " << X << " : " << 
+        gradient(X) << std::endl; 
 
     return 0;
 }
+
+// end of file
