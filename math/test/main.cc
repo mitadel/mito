dfdfd0222bff2dc61d5660f4d89779dc045e10b2
Bianca Giovanardi <biancagi@mit.edu>
2020-11-24 15:48:36 +0100

    math/tests: Improved readability of main.cc

--- math/test/main.cc
+++ math/test/main.cc
@@ -1,44 +1,49 @@
 #include <cmath>
 #include "../math.h"
 
+using mito::function;
+using mito::vector;
+using mito::real;
+using mito::DIM2;
+
 int main () {
 
-    // a function 
-    mito::function<mito::vector<mito::DIM2>, mito::real > f{ 
-        [](const mito::vector<mito::DIM2>& x){ return cos(x[0] * x[1]); } };
+    // a scalar function 
+    function<vector<DIM2>, real > f{ 
+        [](const vector<DIM2>& x){ return cos(x[0] * x[1]); } };
 
     // df/dx[0]
-    mito::function<mito::vector<mito::DIM2>, mito::real > Dx{ 
-        [](const mito::vector<mito::DIM2>& x){ return -sin(x[0] * x[1])* x[1]; } };
+    function<vector<DIM2>, real > Dx{ 
+        [](const vector<DIM2>& x){ return -sin(x[0] * x[1])* x[1]; } };
 
     // df/dx[1] 
-    mito::function<mito::vector<mito::DIM2>, mito::real > Dy{ 
-        [](const mito::vector<mito::DIM2>& x){ return -sin(x[0] * x[1])* x[0]; } };
+    function<vector<DIM2>, real > Dy{ 
+        [](const vector<DIM2>& x){ return -sin(x[0] * x[1])* x[0]; } };
 
     // its partial derivatives 
-    std::array<mito::function<mito::vector<mito::DIM2>, mito::real>, mito::DIM2> Df = {Dx, Dy};
+    std::array<function<vector<DIM2>, real>, DIM2> Df = {Dx, Dy};
 
     // instantiate a scalar function object
-    mito::ScalarField<mito::DIM2> cosine(f, Df);
+    mito::ScalarField<DIM2> cosine(f, Df);
 
     // a point in the reference configuration
-    mito::vector<mito::DIM2> X = {0.0, 0.0};
+    vector<DIM2> X = {0.0, 0.0};
 
     // evaluate f and its gradient
     std::cout << "Evaluating cosine function cos(x[0] * x[1]) at X = " << X << " : " << 
         cosine(X) << std::endl; 
     std::cout << "Evaluating gradient of cosine function at X = " << X << " : " << 
-        mito::Grad<mito::DIM2>(cosine, X) << std::endl; 
+        mito::Grad<DIM2>(cosine, X) << std::endl; 
 
     // instantiate a vector function object
-    mito::VectorField<2, mito::DIM2> cosineVector({cosine, cosine});
+    mito::VectorField<2, DIM2> cosineVector({cosine, cosine});
 
     // evaluate divergence
     std::cout << "Evaluating divergence of cosine vector function at X = " << X << " : " << 
-        mito::Div<mito::DIM2>(cosineVector, X) << std::endl; 
+        mito::Div<DIM2>(cosineVector, X) << std::endl; 
 
-    // 
-    mito::VectorField<2, mito::DIM2> gradient = mito::Grad<mito::DIM2>(cosine);
+    // build vector field with gradient of cosine function 
+    mito::VectorField<2, DIM2> gradient = mito::Grad<DIM2>(cosine);
     std::cout << "Evaluating gradient of cosine function at X = " << X << " : " << 
         gradient(X) << std::endl; 
 
