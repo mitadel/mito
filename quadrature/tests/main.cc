3c4be894ccae18ef2b6e1dc998129f04ddbce3f7
Bianca Giovanardi <biancagi@mit.edu>
2021-02-23 22:09:05 +0100

    quadrature/tests: Updated broken test with the latest changes in quadrature.h

--- quadrature/tests/main.cc
+++ quadrature/tests/main.cc
@@ -2,6 +2,7 @@
 #include "../quadrature.h"
 
 using mito::DIM2;
+using mito::TRI;
 
 int main () {
 
@@ -15,20 +16,20 @@ int main () {
     verticesElement[2][1] = 0.5; 
 
     std::cout << "One-point quadrature rule (r = 1) : " << std::endl;
-    mito::QuadRuleTri1 quadratureRule1;
+    const auto quadratureRule1 = mito::QuadratureRule<TRI, 1>();
     std::cout << "Quad Points (reference): " << quadratureRule1.points() << std::endl;
     std::cout << "Quad Weights: " << quadratureRule1.weights() << std::endl;
 
-    std::vector<mito::vector<DIM2> > quadCoordinates1 = 
+    const std::vector<mito::vector<DIM2> > quadCoordinates1 = 
         quadratureRule1.quadraturePointsCurrentElement(verticesElement);
     std::cout << "Quad Points (element) " << quadCoordinates1 << std::endl;
 
     std::cout << "Three-point quadrature rule (r = 2):" << std::endl;
-    mito::QuadRuleTri2 quadratureRule2;
+    const auto quadratureRule2 = mito::QuadratureRule<TRI, 2>();
     std::cout << "Quad Points (reference): " << quadratureRule2.points() << std::endl;
     std::cout << "Quad Weights: " << quadratureRule2.weights() << std::endl;
 
-    std::vector<mito::vector<DIM2> > quadCoordinates2 = 
+    const std::vector<mito::vector<DIM2> > quadCoordinates2 = 
         quadratureRule2.quadraturePointsCurrentElement(verticesElement);
     std::cout << "Quad Points (element) " << quadCoordinates2 << std::endl;
 
