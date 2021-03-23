71b429c56498b0ebdf13a28c07d3f9a2b3b431f2
Bianca Giovanardi <biancagi@mit.edu>
2021-03-23 20:04:05 +0100

    elements.h: Generalized jacobians calculation
    In the previous implementation method _computeJacobians only worked for simplices (i.e. tetrahedra in 3D, triangles in 2D, segments in 1D, ...).
    The method is now generalized to support implementations for segments in 2D, 3D (implemented), and triangles in 3D (to be implemented).
    
    Co-authored-by: Anwar Koshakji <anwar.koshakji@gmail.com>

--- elements/elements.h
+++ elements/elements.h
@@ -89,6 +89,96 @@ class Connectivity {
     std::vector<int> _connectivityArray; 
 };
 
+template<DIM D>
+void computeSimplicesVolume(const std::vector<mito::vector<D> > & vertices, 
+    std::vector<real> & volumes) 
+{
+    // number of vertices
+    constexpr int V = int(D)+1;
+
+    static tensor<mito::DIM(V)> verticesTensor;
+
+    // get number of elements
+    int nElements = volumes.size();
+
+    // TOFIX: I am not supposed to know here the memory layout of vertices
+    for (auto e = 0; e < nElements; ++e) {
+        std::fill(verticesTensor.begin(), verticesTensor.end(), 0.0);
+        for (auto a = 0; a < V; ++a) {
+            for (auto d = 0; d < D; ++d) {
+                verticesTensor[a * V + d] = vertices[e * V + a][d];
+            }
+            verticesTensor[a * V + D] = 1.0;
+        }
+        volumes[e] = fabs(ComputeDeterminant(verticesTensor)) / Factorial<D>();
+    }
+
+    // all done
+    return;
+}
+
+template<DIM D>
+void computeSegmentsLength(const std::vector<mito::vector<D> > & vertices, 
+    std::vector<real> & lengths) 
+{
+    // number of vertices
+    constexpr int V = 2;
+
+    // get number of elements
+    int nElements = lengths.size();
+
+    // TOFIX: I am not supposed to know here the memory layout of vertices
+    for (auto e = 0; e < nElements; ++e) {
+        real dist = 0.0;
+        for (auto d = 0; d < D; ++d) {
+            real dist_d = vertices[e * V + 0][d] - vertices[e * V + 1][d];
+            dist = dist_d * dist_d;
+        }
+        lengths[e] = sqrt(dist);
+    }
+    // all done
+    return;
+}
+
+template<int V, DIM D>
+void computeElementsVolume(const std::vector<mito::vector<D> > & vertices, 
+    std::vector<real> & volumes);
+
+template<>
+void computeElementsVolume<4, DIM3>(const std::vector<mito::vector<DIM3> > & vertices, 
+    std::vector<real> & volumes)
+{
+    return computeSimplicesVolume<DIM3>(vertices, volumes);
+}
+
+template<>
+void computeElementsVolume<3, DIM2>(const std::vector<mito::vector<DIM2> > & vertices, 
+    std::vector<real> & volumes)
+{
+    return computeSimplicesVolume<DIM2>(vertices, volumes);
+}
+
+template<>
+void computeElementsVolume<2, DIM2>(const std::vector<mito::vector<DIM2> > & vertices, 
+    std::vector<real> & volumes)
+{
+    return computeSegmentsLength<DIM2>(vertices, volumes);
+}
+
+template<>
+void computeElementsVolume<2, DIM3>(const std::vector<mito::vector<DIM3> > & vertices, 
+    std::vector<real> & volumes)
+{
+    return computeSegmentsLength<DIM3>(vertices, volumes);
+}
+
+template<>
+void computeElementsVolume<2, DIM1>(const std::vector<mito::vector<DIM1> > & vertices, 
+    std::vector<real> & volumes) 
+{
+    return computeSimplicesVolume<DIM1>(vertices, volumes);
+}
+
 // Collection of homogeneous elements
 template <class ElementType, DIM D>
 class Elements {
@@ -120,22 +210,9 @@ class Elements {
     // fabs(det(a1, 1; ...; aV, 1)) / D!, with D = V - 1 being the dimension of the phyisical space
     void _computeJacobians() {
         
-        static tensor<mito::DIM(D + 1)> verticesTensor;
+        computeElementsVolume<V /* nVertices */, D /* spatial dim*/>(_vertices, _jacobians);
 
-        // TOFIX: This formula does not work for triangles in 3D, or for lines in 2D or 3D...
-        static_assert(V == int(D)+1);
-
-        for (auto e = 0; e < _nElements; ++e) {
-            std::fill(verticesTensor.begin(), verticesTensor.end(), 0.0);
-            for (auto a = 0; a < V; ++a) {
-                for (auto d = 0; d < D; ++d) {
-                    verticesTensor[a * V + d] = vertex(e, a)[d];
-                }
-                verticesTensor[a * V + D] = 1.0;
-            }
-            _jacobians[e] = fabs(ComputeDeterminant(verticesTensor)) / Factorial<D>();
-        }
-
+        // all done
         return;
     }
 
