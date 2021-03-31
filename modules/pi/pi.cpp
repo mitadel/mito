43eb6a067fe3e740bbcc30a8972b45cef6b914e2
Bianca Giovanardi <biancagi@mit.edu>
2021-03-31 19:37:06 +0200

    modules/pi/pi.cpp: Impoved intentation

--- modules/pi/pi.cpp
+++ modules/pi/pi.cpp
@@ -15,17 +15,17 @@ export module Pi;
 // we can have namespaces within modules
 namespace numbers {
 
-// this helper function lives locally in this file because it is not exported 
-double applyCrunchFactor(double number) {
-    return number * CRUNCH_FACTOR;
-}
+    // this helper function lives locally in this file because it is not exported 
+    double applyCrunchFactor(double number) {
+        return number * CRUNCH_FACTOR;
+    }
 
-// this function is exported, so it is visible outside, when we import this module 
-export double crunch(double number) {
-    auto crunched = applyCrunchFactor(number);
-    std::cout << "Crunched " << number << " with result of " << crunched << std::endl;
-    return crunched;
-}
+    // this function is exported, so it is visible outside, when we import this module 
+    export double crunch(double number) {
+        auto crunched = applyCrunchFactor(number);
+        std::cout << "Crunched " << number << " with result of " << crunched << std::endl;
+        return crunched;
+    }
 
 } // end of namespace
 
