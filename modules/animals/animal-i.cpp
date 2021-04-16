e1b11b1666f721ac4f137aa9429bbb04f67a34ba
Bianca Giovanardi <biancagi@mit.edu>
2021-04-16 19:28:37 +0200

    modules/animals: Added support for gcc11
    It seems gcc11 does not support modules with namespaces yet. In fact, gcc11 gives an error when Cat is under the namespace mito. The compilation works correctly when Cat is declared under no namespace.

---
+++ modules/animals/animal-i.cpp
@@ -0,0 +1,24 @@
+// declare the module
+module;
+
+// declare the module and export it
+export module Animal;
+
+// we can have namespaces within modules
+namespace mito {
+
+    // this class is exported, so it is visible outside, when we import this module
+    export class Animal {
+
+      public:
+        Animal() {}
+        ~Animal() {}
+
+        virtual void hello() const = 0;
+
+        virtual void aFunction() const;
+    };
+
+}    // end of namespace
+
+// end of file
