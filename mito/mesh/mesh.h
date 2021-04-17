cae304f2acc2089e08487793f5334b782952661d
Bianca Giovanardi <biancagi@mit.edu>
2021-04-17 09:41:27 +0200

    mito/mesh: Added data structure for entities compositions
    This container will be used to avoid double insertion of the same entity (e.g. an edge or a face) shared by more than element. Also this container can be used for look-up of edges, faces, and volumes based on vertices, edges, and faces, respectively.

--- mito/mesh/mesh.h
+++ mito/mesh/mesh.h
@@ -31,8 +31,31 @@ namespace mito {
         //      entity_collection<Simplex<D>>
         using entities_tuple_t = typename entities_tuple<>::type;
 
+      private:
+        // typedef for a composition map of mesh entities:
+        // these maps map:
+        //      2 pointers to nodes into a pointer to edge,
+        //      3 pointers to edges into a pointer to face, ...
+        // std::map<std::array<Simplex<0> *, 2>, Simplex<1> *>  edges composition
+        // std::map<std::array<Simplex<1> *, 3>, Simplex<2> *>  faces compositions
+        // std::map<std::array<Simplex<2> *, 4>, Simplex<3> *>  volumes compositions
+        template <size_t I>
+        using composition_map = std::map<std::array<Simplex<I> *, I + 2>, Simplex<I + 1> *>;
+
+        template <typename = std::make_index_sequence<D>>
+        struct composition_tuple;
+
+        template <size_t... I>
+        struct composition_tuple<std::index_sequence<I...>> {
+            using type = std::tuple<composition_map<I>...>;
+        };
+
+        // this expands to:
+        // tuple<composition_map<0>, composition_map<1>, ..., composition_map<D-1>
+        using composition_tuple_t = typename composition_tuple<>::type;
+
       public:
-        Mesh(std::string meshFileName) : _entities(), _edges() { _loadMesh(meshFileName); }
+        Mesh(std::string meshFileName) : _entities(), _compositions() { _loadMesh(meshFileName); }
 
         ~Mesh() {}
 
@@ -43,6 +66,9 @@ namespace mito {
       private:
         // container to store D+1 containers of d dimensional entities with d = 0, ..., D
         entities_tuple_t _entities;
+        // container to store D maps with the composition of (i+1)-dimensional entities in terms
+        // of arrays of i-dimensional entities
+        composition_tuple_t _compositions;
     };
 
 }
