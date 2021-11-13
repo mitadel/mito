1cdfa4756445c3d30fc51ae103cab84289416471
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2021-11-13 13:57:08 +0100

    symmetric-packing: remove template wrt index and D

--- symmetric-packing/main.cc
+++ symmetric-packing/main.cc
@@ -3,144 +3,109 @@
 #include <array>
 #include <cassert>
 
-/*
 int entries(int N, int D)
 {
     if (N == 1) return D;
 
-    int number = 0;
+    int sum_entries = 0;
     for (int i = 0; i < D; ++i)
     {
-        number += entries(N - 1, D - i);
+        sum_entries += entries(N - 1, D - i);
     }
 
-    return number;
-}*/
+    return sum_entries;
 
-template<int D> 
-int entries(int N)
-{
-    if (N == 1) return D;
-
-    // helper function (sum all entries for i = 0, ..., D - 1)
-    constexpr auto _sum_entries = []<size_t... i>(int N, std::index_sequence<i...>)
-    {
-        // all done
-        return (entries<D-i>(N-1) + ...);
-    };
-
-    return _sum_entries(N, std::make_index_sequence<D> {});
 }
 
-// template<int D, int N, class... T> 
-// int offset(T... args) requires (sizeof...(T) == N)
-// {
-//     return args[0];
-// }
-
-// helper function to compute the product of an arguments list
-template <typename... Args>
-static constexpr auto sum(Args &&... args) -> int
+template<int N> 
+int offset1(int D, int i)
 {
-    if constexpr (sizeof...(args))
-        return  (args + ...);
-    return 0;
-}
-
-template<int D, int N, int i> 
-int offset1()
-{
-    static_assert(N==1);
+    static_assert(N == 1);
     return i;
 }
 
-template<int D, int N, int i, int j> 
-int offset2()
+template<int N> 
+int offset2(int D, int i, int j)
 {
-    static_assert(N==2);
-    // helper function (sum all entries for i = 0, ..., D - 1)
-    constexpr auto _sum_entries = []<size_t... a>(std::index_sequence<a...>)
+    static_assert(N == 2);
+    int sum_entries = 0;
+    for (int a = 0; a < i; ++a)
     {
-        // all done
-        return sum(entries<D - a>(N - 1) ...);
-    };
+        sum_entries += entries(N - 1, D - a);
+    }
 
-    // std::cout << "_sum_entries = " << _sum_entries(std::make_index_sequence<i> {}) << std::endl;
-    return _sum_entries(std::make_index_sequence<i> {}) + offset1<D - i, N - 1, j>();
+    return sum_entries + offset1<N - 1>(D - i, j);
+
 }
 
-template<int D, int N, int i, int j> 
-int offset2_wrapped()
+template<int D, int N> 
+int offset2_symmetric(int i, int j)
 {
-    static_assert(N==2);
-    // sort indices from min to max
-    static_assert(i <= j);
-    return offset2<D, N, i, j - i>();
+    static_assert(N == 2);
+    // assert indices are sorted
+    assert(i <= j);
+    return offset2<N>(D, i, j - i);
 }
 
-template<int D, int N, int i, int j, int k> 
-int offset3()
+template<int N> 
+int offset3(int D, int i, int j, int k)
+{
+    static_assert(N == 3);
+    int sum_entries = 0;
+    for (int a = 0; a < i; ++a)
+    {
+        sum_entries += entries(N - 1, D - a);
+    }
+
+    return sum_entries + offset2<N - 1>(D - i, j, k);
+}
+
+template<int D, int N> 
+int offset3_symmetric(int i, int j, int k)
 {
     static_assert(N==3);
-    // helper function (sum all entries for i = 0, ..., D - 1)
-    constexpr auto _sum_entries = []<size_t... a>(std::index_sequence<a...>)
-    {
-        // all done
-        return sum(entries<D - a>(N - 1) ...);
-    };
-
-    return _sum_entries(std::make_index_sequence<i> {}) + offset2<D - i, N - 1, j, k>();
-}
-
-template<int D, int N, int i, int j, int k> 
-int offset3_wrapped()
-{
-    static_assert(N==3);
-    // constexpr std::array<int, 3> a = {i, j, k};
-    // // sort indices from min to max
-    // std::sort(a.begin(), a.end());
-    //return offset3<D, N, a[0], a[1] - a[0], a[2] - a[1]>();
-    static_assert(i <= j && j <= k);
-    return offset3<D, N, i, j - i, k - j>();
+    // assert indices are sorted
+    assert(i <= j && j <= k);
+    return offset3<N>(D, i, j - i, k - j);
 }
 
 int main() {
 
-    assert(entries<2 /* D */>(2 /* N */) == 3);
-    assert(entries<2 /* D */>(3 /* N */) == 4);
-    assert(entries<3 /* D */>(2 /* N */) == 6);
-    assert(entries<3 /* D */>(3 /* N */) == 10);
+    assert(entries(2 /* N */, 2 /* D */) == 3);
+    assert(entries(3 /* N */, 2 /* D */) == 4);
+    assert(entries(2 /* N */, 3 /* D */) == 6);
+    assert(entries(3 /* N */, 3 /* D */) == 10);
 
-#if 1
+#if 0
     int offset = 0;
 
     std::cout << "(0, 0) " << std::endl;
-    offset = offset2_wrapped<3 /* D */, 2 /* N */, 0, 0>();
+    offset = offset2_symmetric<3 /* D */, 2 /* N */, 0, 0>();
     std::cout << "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 1) " << std::endl;
-    offset = offset2_wrapped<3 /* D */, 2 /* N */, 0, 1>();
+    offset = offset2_symmetric<3 /* D */, 2 /* N */, 0, 1>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 2) " << std::endl;
-    offset = offset2_wrapped<3 /* D */, 2 /* N */, 0, 2>();
+    offset = offset2_symmetric<3 /* D */, 2 /* N */, 0, 2>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(1, 1) " << std::endl;
-    offset = offset2_wrapped<3 /* D */, 2 /* N */, 1, 1>();
+    offset = offset2_symmetric<3 /* D */, 2 /* N */, 1, 1>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(1, 2) " << std::endl;
-    offset = offset2_wrapped<3 /* D */, 2 /* N */, 1, 2>();
+    offset = offset2_symmetric<3 /* D */, 2 /* N */, 1, 2>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(2, 2) " << std::endl;
-    offset = offset2_wrapped<3 /* D */, 2 /* N */, 2, 2>();
+    offset = offset2_symmetric<3 /* D */, 2 /* N */, 2, 2>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 #endif
@@ -149,75 +114,75 @@ int main() {
     int offset = 0;
 
     std::cout << "(0, 0, 0) " << std::endl;
-    offset = offset3_wrapped<2 /* D */, 3 /* N */, 0, 0, 0>();
+    offset = offset3_symmetric<2 /* D */, 3 /* N */, 0, 0, 0>();
     std::cout << "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 0, 1) " << std::endl;
-    offset = offset3_wrapped<2 /* D */, 3 /* N */, 0, 0, 1>();
+    offset = offset3_symmetric<2 /* D */, 3 /* N */, 0, 0, 1>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 1, 1) " << std::endl;
-    offset = offset3_wrapped<2 /* D */, 3 /* N */, 0, 1, 1>();
+    offset = offset3_symmetric<2 /* D */, 3 /* N */, 0, 1, 1>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(1, 1, 1) " << std::endl;
-    offset = offset3_wrapped<2 /* D */, 3 /* N */, 1, 1, 1>();
+    offset = offset3_symmetric<2 /* D */, 3 /* N */, 1, 1, 1>();
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 #endif
-#if 0
+#if 1
     int offset = 0;
 
     std::cout << "(0, 0, 0) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 0, 0, 0>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */> (0, 0, 0);
     std::cout << "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 0, 1) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 0, 0, 1>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(0, 0, 1);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 0, 2) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 0, 0, 2>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(0, 0, 2);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 1, 1) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 0, 1, 1>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(0, 1, 1);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 1, 2) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 0, 1, 2>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(0, 1, 2);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(0, 2, 2) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 0, 2, 2>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(0, 2, 2);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(1, 1, 1) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 1, 1, 1>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(1, 1, 1);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(1, 1, 2) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 1, 1, 2>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(1, 1, 2);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(1, 2, 2) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 1, 2, 2>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(1, 2, 2);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 
     std::cout << "(2, 2, 2) " << std::endl;
-    offset = offset3_wrapped<3 /* D */, 3 /* N */, 2, 2, 2>();
+    offset = offset3_symmetric<3 /* D */, 3 /* N */>(2, 2, 2);
     std::cout <<  "offset " << offset << std::endl;
     std::cout << std::endl;
 #endif
