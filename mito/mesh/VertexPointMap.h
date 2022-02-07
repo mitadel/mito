50df269e39e51256b690692fbc71c12ec0b4a896
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-02-07 20:28:18 +0100

    mito/mesh: {VertexPointMap} is now the owner of the points memory

--- mito/mesh/VertexPointMap.h
+++ mito/mesh/VertexPointMap.h
@@ -11,7 +11,7 @@ namespace mito {
 
         // using map_t = std::unordered_map<std::reference_wrapper<vertex_t>, point_t<D>>;
         // TODO: test O for access
-        using map_t = std::unordered_map<const vertex_t *, const point_t<D> *>;
+        using map_t = std::unordered_map<const vertex_t *, const point_t<D>>;
 
       public:
         VertexPointMap() : _map() {};
@@ -37,7 +37,7 @@ namespace mito {
             // iterate on map
             for (auto item : _map) {
                 std::cout << "Vertex: " << item.first << std::endl;
-                std::cout << "Point: " << *item.second << std::endl;
+                std::cout << "Point: " << item.second << std::endl;
             }
             // all done
             return;
@@ -49,19 +49,26 @@ namespace mito {
         {
             return _map.insert(
                 // std::pair<std::reference_wrapper<vertex_t>, point_t<D>>(vertex, point)
-                std::pair<const vertex_t *, const point_t<D> *>(&vertex, &point));
+                std::pair<const vertex_t *, const point_t<D>>(&vertex, point));
+        }
+
+        auto insert(const vertex_t & vertex, const point_t<D> && point)
+        {
+            return _map.insert(
+                // std::pair<std::reference_wrapper<vertex_t>, point_t<D>>(vertex, point)
+                std::pair<const vertex_t *, const point_t<D>>(&vertex, point));
         }
 
         const point_t<D> & operator[](const vertex_t * vertex) const
         {
             auto point = _map.find(vertex);
-            return *(point->second);
+            return point->second;
         }
 
         const point_t<D> & operator[](const vertex_t & vertex) const
         {
             auto point = _map.find(&vertex);
-            return *(point->second);
+            return point->second;
         }
 
       private:
