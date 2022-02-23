771e3f09a2fd0d04c5833c8eebdd95a44a3b7e6c
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-02-23 16:45:33 +0100

    mito/tests/mesh: test now uses api

--- mito/tests/mesh/main.cc
+++ mito/tests/mesh/main.cc
@@ -7,22 +7,20 @@ int
 main()
 {
     // load mesh
-    mito::mesh::Mesh<2> mesh("square.summit");
-    // instantiate an ElementSet as a collection of simplices and a vertex-coordinates mapping.
+    auto mesh = mito::mesh::mesh<2>("square.summit");
+    // instantiate a element set as a collection of simplices and vertices.
     const auto & elements = mesh.elements<2>(); //TODO: region label to fetch elements
     const auto & vertices = mesh.vertices();
-    mito::mesh::ElementSet elementSet(elements, vertices);
+    auto elementSet = mito::mesh::element_set(elements, vertices);
 
     // instantiate a scalar field
-    mito::math::scalar_field_t<2> f_cosine(
-        [](const mito::vector_t<2> & x) { return cos(x[0] * x[1]); });
+    auto f_cosine = 
+        mito::math::scalar_field([](const mito::vector_t<2> & x) { return cos(x[0] * x[1]); });
 
     // instantiate a GAUSS integrator with degree of exactness equal to 2
-    mito::quadrature::integrator_t<
-        mito::quadrature::GAUSS, 2 /* degree of exactness */, mito::mesh::ElementSet<mito::mesh::triangle_t, 2>>
-        integrator(elementSet);
+    auto integrator = mito::quadrature::integrator<mito::quadrature::GAUSS, 2>(elementSet);
 
-    mito::real result = integrator.integrate(f_cosine);
+    auto result = integrator.integrate(f_cosine);
     std::cout << "Integration of cos(x*y): Result = " << result
               << ", Error = " << std::fabs(result - 0.946083) << std::endl;
     assert(std::fabs(result - 0.946083) < 1.e-7);
