94e99d6af04bc7b65dec9383ee4679c865057c47
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-02-27 17:02:57 +0100

    mito/test: add test for timing of algebraic operations

---
+++ mito/tests/algebra/main.cc
@@ -0,0 +1,29 @@
+#include "../../base.h"
+
+int
+main(int argc, char ** argv)
+{
+    /*constexpr*/ mito::matrix_t<3> Du { 1.0, 2.0, 3.0, 1.0, 2.0, 3.0, 1.0, 2.0, 1.0 };
+
+    int i = 0;
+    int N = 10000000;
+    while (i < N) {
+#if 1
+        /*constexpr*/ mito::matrix_t<3> A = Du + Du;
+#else
+        mito::matrix_t<3> A;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                //for (int k = 0; k < 3; k++) {
+                    A[i * 3 + j] = Du[i * 3 + j] + Du[i * 3 + j];
+                //}
+            }
+        }
+#endif
+            ++ i;
+        //std::cout << A << std::endl;
+    }
+
+    return 0;
+}
