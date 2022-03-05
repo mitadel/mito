9f93689f3176771802d5f3dada7d8a2323839be3
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-03-05 08:50:49 +0100

    mito/tests/gent: remove redundant template paramater in {Constitutive} call

--- mito/tests/gent/main.cc
+++ mito/tests/gent/main.cc
@@ -12,12 +12,12 @@ main(int argc, char ** argv)
     mito::vector_t<3> u = { 0.0, 0.0, 0.0 };
     mito::matrix_t<3> Du = { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 };
     mito::matrix_t<3> P = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-    material.Constitutive<3>(u, Du, P);
+    material.Constitutive(u, Du, P);
 
     mito::vector_t<2> u2 = { 0.0, 0.0 };
     mito::matrix_t<2> Du2 = { 1.0, 0.0, 0.0, 1.0 };
     mito::matrix_t<2> P2 = { 0.0, 0.0, 0.0, 0.0 };
-    material.Constitutive<2>(u2, Du2, P2);
+    material.Constitutive(u2, Du2, P2);
 
     std::cout << "P = " << P << std::endl;
     std::cout << "P2 = " << P2 << std::endl;
