f1a5fab07889297dc3f37553441285b0da08cf1b
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-03-19 09:55:53 +0100

    mito/mesh: {OrientedSimplexFactory} is now a factory for simplices of dimension D only.
    Break {OrientedSimplexFactory} down to one factory for each simplex dimension, as opposed to one single factory for all admissible dimensions in a physical space of dimension D.
    This allows to simplify the data structures in {OrientedSimplexFactory}.
    In addition this allows to remove the physical space dimension from a purely topological object.

--- mito/mesh/OrientedSimplexFactory.h
+++ mito/mesh/OrientedSimplexFactory.h
@@ -3,61 +3,40 @@
 #define mito_mesh_OrientedSimplexFactory_h
 
 namespace mito::mesh {
-    // TODO: forbid user to create simplices without passing through the factory
-    template <int D>    // TOFIX: not sure we need this template parameter
+    template <int D>
     class OrientedSimplexFactory {
       private:
         // typedef for simplex composition
-        template <int I>
-        using simplex_composition_t = Simplex<I>::simplex_composition_t;
+        using simplex_composition_t = Simplex<D>::simplex_composition_t;
 
         // typedef for an orientation map of simplices:
-        // these maps map:
-        //      a simplex pointer and a boolean to an oriented simplex pointer 
-        template <int I>
-        using orientation_map = std::map<std::tuple<simplex_t<I> *, bool>, 
-            oriented_simplex_t<I> *>;
-
-        template <typename = std::make_index_sequence<D>>
-        struct orientation_tuple_t;
-
-        template <size_t... I>
-        struct orientation_tuple_t<std::index_sequence<I...>> {
-            using type = std::tuple<orientation_map<int(I + 1)>...>;
-        };
-
-        // this expands to:
-        // tuple<orientation_map<1>, ..., orientation_map<D>
-        using orientation_map_t = typename orientation_tuple_t<>::type;
+        // this map maps a simplex pointer and a boolean to an oriented simplex pointer
+        using orientation_map_t =
+            std::map<std::tuple<simplex_t<D> *, bool>, oriented_simplex_t<D> *>;
 
       public:
         OrientedSimplexFactory() = delete;
 
-        template <int I>
-        static oriented_simplex_t<I> * OrientedSimplex(const simplex_composition_t<I> & composition)
+        static oriented_simplex_t<D> * OrientedSimplex(const simplex_composition_t & composition)
         {
             // get from the factory the representative of simplices with this composition
-            auto * simplex = SimplexFactory<D>::template Simplex<I>(composition);
+            auto * simplex = SimplexFactory<D>::template Simplex<D>(composition);
 
             // compute the orientation of the current composition with respect to the representative
             bool orientation = _orientation(composition, *simplex);
 
             // look up for an oriented simplex with opposite orientation
-            auto ret_find =
-                std::get<I - 1>(_orientations).find(std::make_tuple(simplex, !orientation));
+            auto ret_find = _orientations.find(std::make_tuple(simplex, !orientation));
 
             // if there was already a simplex with opposite orientation... 
-            if (ret_find != std::get<I - 1>(_orientations).end()) {
+            if (ret_find != _orientations.end()) {
                 // report
                 std::cout << "Found opposite orientation..." << std::endl;
                 // ... then insert a new oriented simplex with same footprint but different
                 // orientation
                 auto mytuple = std::make_tuple(simplex, orientation);
-                auto ret_emplace =
-                    std::get<I - 1>(_orientations)
-                        .emplace(
-                            mytuple,
-                            new oriented_simplex_t<I>(ret_find->second->footprint(), orientation));
+                auto ret_emplace = _orientations.emplace(
+                    mytuple, new oriented_simplex_t<D>(ret_find->second->footprint(), orientation));
                 // report
                 if (ret_emplace.second == false) {
                     std::cout << "Duplicate orientation: Not inserted..." << std::endl;
@@ -74,8 +53,7 @@ namespace mito::mesh {
                 // orientation
                 auto mytuple = std::make_tuple(simplex, orientation);
                 auto ret_emplace =
-                    std::get<I - 1>(_orientations)
-                        .emplace(mytuple, new oriented_simplex_t<I>(simplex, orientation));
+                    _orientations.emplace(mytuple, new oriented_simplex_t<D>(simplex, orientation));
 
                 // report
                 if (ret_emplace.second == false) {
@@ -89,29 +67,36 @@ namespace mito::mesh {
 
       private:
         // compute the orientation of the {composition} with respect to the orientation of {simplex}
-        static bool _orientation(simplex_composition_t<1> composition, const simplex_t<1> & simplex)
-        {
-            if (composition == simplex.simplices()) {
-                return true;
-            }
-            return false;
-        }
-
-        // compute the orientation of the {composition} with respect to the orientation of {simplex}
-        static bool _orientation(simplex_composition_t<2> composition, const simplex_t<2> & simplex)
-        {
-            auto first_simplex = std::min_element(composition.begin(), composition.end());
-            std::rotate(composition.begin(), first_simplex, composition.end());
-            if (composition == simplex.simplices()) {
-                return true;
-            }
-            return false;
-        }
+        static bool _orientation(simplex_composition_t composition, const simplex_t<D> & simplex);
 
       private:
-        // container to ... //TOFIX
+        // container to store the relation (simplex, orientation) -> oriented simplex
         static orientation_map_t _orientations;
     };
+
+    // compute the orientation of the {composition} with respect to the orientation of {simplex}
+    template <>
+    bool OrientedSimplexFactory<1>::_orientation(
+        OrientedSimplexFactory<1>::simplex_composition_t composition, const simplex_t<1> & simplex)
+    {
+        if (composition == simplex.simplices()) {
+            return true;
+        }
+        return false;
+    }
+
+    // compute the orientation of the {composition} with respect to the orientation of {simplex}
+    template <>
+    bool OrientedSimplexFactory<2>::_orientation(
+        OrientedSimplexFactory<2>::simplex_composition_t composition, const simplex_t<2> & simplex)
+    {
+        auto first_simplex = std::min_element(composition.begin(), composition.end());
+        std::rotate(composition.begin(), first_simplex, composition.end());
+        if (composition == simplex.simplices()) {
+            return true;
+        }
+        return false;
+    }
 
     // initialize static attribute
     template <int D>
