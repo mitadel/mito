e65af1504f469afa8cf251187c2329aa7b830c5d
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-03-20 18:00:24 +0100

    mito/tests: fix test (adjusted to new interface)

--- mito/tests/oriented-simplex/main.cc
+++ mito/tests/oriented-simplex/main.cc
@@ -9,24 +9,24 @@ main()
     mito::mesh::vertex_t vertex1;
 
     // instantiate an oriented segment
-    auto * oriented_segment0 = 
+    auto & oriented_segment0 = 
         mito::mesh::OrientedSimplexFactory<1>::OrientedSimplex({ &vertex0, &vertex1 });
     // assert there is only one pointer to the simplex footprint
-    assert(oriented_segment0->footprint().use_count() == 1);
+    assert(oriented_segment0.footprint().use_count() == 1);
 
     // instantiate an identical oriented segment
-    auto * oriented_segment1 = 
+    auto & oriented_segment1 = 
         mito::mesh::OrientedSimplexFactory<1>::OrientedSimplex({&vertex0, &vertex1 });
     // assert the factory returned the same oriented segment
-    assert(oriented_segment0 == oriented_segment1);
+    assert(&oriented_segment0 == &oriented_segment1);
     // assert there is still only one pointer to the simplex footprint
-    assert(oriented_segment0->footprint().use_count() == 1);
+    assert(oriented_segment0.footprint().use_count() == 1);
 
     // instantiate an oriented segment with opposite orientation
-    auto * oriented_segment2 =
+    auto & oriented_segment2 =
         mito::mesh::OrientedSimplexFactory<1>::OrientedSimplex({ &vertex1, &vertex0 });
     // assert there are now two pointers to the simplex footprint
-    assert(oriented_segment0->footprint().use_count() == 2);
+    assert(oriented_segment0.footprint().use_count() == 2);
 
     // all done
     return 0;
