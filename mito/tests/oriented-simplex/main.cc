36fa2fa3c232db8c68761585a91ece9120369ad3
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-03-24 07:46:02 +0100

    mito/tests/oriented-simplex: test now uses api

--- mito/tests/oriented-simplex/main.cc
+++ mito/tests/oriented-simplex/main.cc
@@ -9,14 +9,12 @@ main()
     mito::mesh::vertex_t vertex1;
 
     // instantiate an oriented segment
-    auto & oriented_segment0 = 
-        mito::mesh::OrientedSimplexFactory<1>::OrientedSimplex({ &vertex0, &vertex1 });
+    auto & oriented_segment0 = mito::mesh::segment({ &vertex0, &vertex1 });
     // assert there is only one pointer to the simplex footprint
     assert(oriented_segment0.use_count() == 1);
 
     // instantiate an identical oriented segment
-    auto & oriented_segment1 = 
-        mito::mesh::OrientedSimplexFactory<1>::OrientedSimplex({&vertex0, &vertex1 });
+    auto & oriented_segment1 = mito::mesh::segment({ &vertex0, &vertex1 });
     // assert the factory returned the same oriented segment
     assert(&oriented_segment0 == &oriented_segment1);
     // assert there is still only one pointer to the simplex footprint
@@ -25,8 +23,7 @@ main()
     assert(&oriented_segment1.flip().flip() == &oriented_segment1); // QUESTION: flip-flip creates the opposite simplex
 
     // instantiate an oriented segment with opposite orientation
-    auto & oriented_segment2 =
-        mito::mesh::OrientedSimplexFactory<1>::OrientedSimplex({ &vertex1, &vertex0 });
+    auto & oriented_segment2 = mito::mesh::segment({ &vertex1, &vertex0 });
     // assert there are now two pointers to the simplex footprint
     assert(oriented_segment0.use_count() == 2);
     // assert that flipping the opposite segment gives the original segment
