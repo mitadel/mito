de83ad4b0371a5c2fdc19a4e6af7b2fea7d84508
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-03-27 19:06:42 +0200

    mito/tests/load-mesh: add access to boundary in test

--- mito/tests/load-mesh/main.cc
+++ mito/tests/load-mesh/main.cc
@@ -8,8 +8,12 @@ main()
 
     //
     t = clock();
-    mito::mesh::Mesh<2> mesh("rectangle.summit");
-    std::cout << "Loaded mesh (without repeated elements) in " << clock() - t << std::endl;
+    auto mesh = mito::mesh::mesh<2>("rectangle.summit");
+    std::cout << "Loaded mesh in " << clock() - t << std::endl;
+
+    t = clock();
+    auto element_set_boundary = mesh.boundary_element_set<1>();
+    std::cout << "Fetched boundary in " << clock() - t << std::endl;
 
     return 0;
 }
