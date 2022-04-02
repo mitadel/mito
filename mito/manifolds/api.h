ee495787a700fb1fa94ee8e174c52cdaf0c161ac
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-04-02 09:32:57 +0200

    mito/manifolds/api.h: fix typo in namespace name

--- mito/manifolds/api.h
+++ mito/manifolds/api.h
@@ -3,24 +3,24 @@
 #define mito_manifolds_api_h
 
 
-namespace mito::mesh {
+namespace mito::manifolds {
 
     // element set factory (from vectors)
     template <class elementT, int D>
     constexpr auto manifold(
-        const std::vector<elementT *> & elements, const point_cloud_t<D> & points);
+        const std::vector<elementT *> & elements, const mito::mesh::point_cloud_t<D> & points);
     template <class elementT, int D>
     constexpr auto manifold(
-        std::vector<elementT *> && elements, const point_cloud_t<D> & points);
+        std::vector<elementT *> && elements, const mito::mesh::point_cloud_t<D> & points);
 
     // element set factory (from sets)
     template <class elementT, int D>
     constexpr auto manifold(
-        const std::unordered_set<elementT *> & elements, const point_cloud_t<D> & points);
+        const std::unordered_set<elementT *> & elements,
+        const mito::mesh::point_cloud_t<D> & points);
     template <class elementT, int D>
     constexpr auto manifold(
-        std::unordered_set<elementT *> && elements, const point_cloud_t<D> & points);
-
+        std::unordered_set<elementT *> && elements, const mito::mesh::point_cloud_t<D> & points);
 }
 
 
