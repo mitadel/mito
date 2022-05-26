78558a797a26ccfb5ff12cdb092e8b52b15969ed
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-05-26 16:36:47 +0200

    segmented-container: add queue to keep track of available locations for writing
    If an element is erased, that element is marked as invalid and its memory location is signed up for reuse in the container.

--- segmented-container/main.cc
+++ segmented-container/main.cc
@@ -3,6 +3,7 @@
 #include <array>
 #include <numeric>
 #include <cmath>
+#include <queue>
 
 template <class T, int N = 10 /* segment size */>
 class SegmentedContainer {
@@ -42,12 +43,25 @@ class SegmentedContainer {
         return segment;
     }
 
-    T * _next_available_location() const
+    T * _next_available_location()
     {
         // if the container is empty
         if (_data.size() == 0) {
             // there is no available location
             return nullptr;
+        }
+
+        if (!_available_locations.empty()) {
+            // get an available location from the queue
+            T * location = _available_locations.front();
+
+            // remove the next available location from the queue
+            // (TOFIX: what if the next available location is then not used?
+            // Maybe we should not pop here)
+            _available_locations.pop();
+
+            // return the available location from the queue
+            return location;
         }
 
         // if the container is all filled up
@@ -79,11 +93,29 @@ class SegmentedContainer {
         // increment the size of the container
         ++_n_elements;
 
-        // increment the index of the furthest element
-        ++_end;
+        // if we have written past the last element
+        if (location == _end) {
+            // move the end of the container
+            ++_end;
+        }
 
         // all done
         return location;
+    }
+
+    void erase(T * element)
+    {
+        // mark element as invalid
+        element->invalidate();
+
+        // decrement the number of elements
+        --_n_elements;
+
+        // add the address of the element to the queue of the available locations for write
+        _available_locations.push(element);
+
+        // all done
+        return;
     }
 
   private:
@@ -93,6 +125,8 @@ class SegmentedContainer {
     T * _end;
     // the number of elements stored in the container
     int _n_elements;
+    // a queue with the available locations for writing
+    std::queue<T *> _available_locations;
 };
 
 class Simplex {
