ae9a52527a77727b759658622de4a204b0b404b2
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-08-01 19:53:24 +0200

    mito/tests/shared-pointer: test is now an actual test
    Testing that the resource is accessible.

--- mito/tests/shared-pointer/main.cc
+++ mito/tests/shared-pointer/main.cc
@@ -3,29 +3,38 @@
 
 class Resource {
   public:
-    Resource(int a, int b, Resource * c) : _a(a)
+    Resource(int a, Resource * c) : _a(a)
     {
-        std::cout << "Built resource " << _a << " " << b << " " << c << std::endl;
+        std::cout << "Built resource " << _a << " " << c << std::endl;
     }
 
     ~Resource() { std::cout << "Destroying resource " << _a << std::endl; }
 
-  private:
     int _a;
 };
 
 int
 main()
 {
-
+    // the type of resource to be stored
     using resource_t = Resource;
+    // the size of the segment of memory
+    constexpr int N = 10;
+    // the initialization value for the resource
+    int a = 10;
 
     // allocate a new segment of memory
-    resource_t * segment = static_cast<resource_t *>(::operator new(10 * sizeof(resource_t)));
+    resource_t * segment = static_cast<resource_t *>(::operator new(N * sizeof(resource_t)));
 
-    resource_t * location = (segment + 1);
-    mito::utilities::SharedPointer<resource_t, true> handle(10, 12, nullptr, location);
+    // select a location within the segment of memory
+    resource_t * location = (segment + 3);
+    // instantiate new resource at {location}
+    mito::utilities::SharedPointer<resource_t, true> handle(a, nullptr, location);
 
+    // assert that the resource is accessible correctly
+    assert(location->_a == a);
+
+    // free the segment of memory
     ::operator delete(segment);
 
     // all done
