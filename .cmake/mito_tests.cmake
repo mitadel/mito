e0aaf444af539580654e31afc9371fe43b60f307
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-08-23 18:34:07 +0200

    .cmake/mito_tests.cmake: remove redundancies in {mito_testsInit}

--- .cmake/mito_tests.cmake
+++ .cmake/mito_tests.cmake
@@ -6,17 +6,12 @@ function(mito_testsInit)
     set(MITO_TESTSUITE_DIR ${CMAKE_SOURCE_DIR}/tests PARENT_SCOPE)
     set(MITO_TESTSUITE_TMPDIR ${CMAKE_BINARY_DIR}/tmp PARENT_SCOPE)
 
-    # get support
-    include(CTest)
-
     # enable test target
     enable_testing()
 
     # print test output in case of failure
     set($ENV{CTEST_OUTPUT_ON_FAILURE} 1)
 
-    # add the testsuite to the pile
-    # add_subdirectory(tests)
     # all done
 endfunction(mito_testsInit)
 
