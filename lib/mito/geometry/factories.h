a2ad0f99b52be49abc6a61d7bcd8ed5baf52a2a6
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-09-18 15:31:34 +0200

    lib/mito/geometry: factory {vertex} now uses the {topology} api

--- lib/mito/geometry/factories.h
+++ lib/mito/geometry/factories.h
@@ -15,8 +15,11 @@ namespace mito::geometry {
     template <int D>
     oriented_simplex_ptr<0> vertex(point_t<D> && point)
     {
-        auto new_vertex = std::make_shared<oriented_simplex_t<0>>();
+        // get a new vertex from the topology
+        auto new_vertex = mito::topology::vertex();
+        // register the new vertex with the point cloud
         point_cloud<D>::insert(new_vertex, point);
+        // return the new vertex
         return new_vertex;
     }
 }
