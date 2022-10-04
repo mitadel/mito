3a76379b0988e570b2beb68efc1ccb95711ec298
Anwar Koshakji <anwar.koshakji@gmail.com>
2022-10-04 18:22:49 +0200

    tests/mito.ext/main.py: Clean up

--- tests/mito.ext/main.py
+++ tests/mito.ext/main.py
@@ -1,13 +1,12 @@
 #! /usr/bin/env python3
 
 import mito.extensions.mito as mito
+from numpy import cos
 
 def myfunction(x):
     return eval('cos(x[0] * x[1])')
 
 field = mito.ScalarField2D(myfunction)
-#x = mito.Vector2D([10., 20.])
-#print(field(x))
 
 elementset = mito.ManifoldTriangle2D("square.summit")
 integrator = mito.GaussIntegrator2Triangle2D(elementset)
@@ -17,3 +16,5 @@ for i in range(0, N):
     result = integrator.integrate(field)
 
 assert abs(result - 0.946083) < 1.e-3
+
+print("all done!")
