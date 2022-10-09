3a74b935f9e73b2e49996b9d80fcf6f6e9d3bd3c
Anwar Koshakji <anwar.koshakji@gmail.com>
2022-10-09 12:08:31 +0200

    tests/oriented-simplex/main.cc: update test
    After the refactoring of cd92a4c, {mito::topology::segment} returns a
    shared pointer. Updated the test accordingly.

--- tests/mito.lib/oriented-simplex/main.cc
+++ tests/mito.lib/oriented-simplex/main.cc
@@ -17,13 +17,13 @@ main()
         // instantiate an identical oriented segment
         auto oriented_segment1 = mito::topology::segment({ vertex0, vertex1 });
         // assert the factory returned the same oriented segment
-        assert(oriented_segment0.simplex_id() == oriented_segment1.simplex_id());
+        assert((*oriented_segment0).simplex_id() == (*oriented_segment1).simplex_id());
         // assert there is still only one pointer to the simplex footprint
         assert(!mito::topology::exists_flipped(oriented_segment0));
         // assert that flipping the flipped simplex gives the original simplex
         assert(
-            mito::topology::flip(mito::topology::flip(oriented_segment1)).simplex_id()
-            == oriented_segment1.simplex_id());
+            (*mito::topology::flip(mito::topology::flip(oriented_segment1))).simplex_id()
+            == (*oriented_segment1).simplex_id());
 
         // instantiate an oriented segment with opposite orientation
         auto oriented_segment2 = mito::topology::segment({ vertex1, vertex0 });
@@ -31,7 +31,8 @@ main()
         assert(mito::topology::exists_flipped(oriented_segment0));
         // assert that flipping the opposite segment gives the original segment
         assert(
-            mito::topology::flip(oriented_segment2).simplex_id() == oriented_segment1.simplex_id());
+            (*mito::topology::flip(oriented_segment2)).simplex_id()
+            == (*oriented_segment1).simplex_id());
     }
     {
         // build vertices
@@ -49,7 +50,7 @@ main()
         // ask factory for the same triangle (with different order of segments)
         auto element_1 = mito::topology::triangle({ segment_b, segment_c, segment_a });
         // assert the factory returned the same object
-        assert(element_0.simplex_id() == element_1.simplex_id());
+        assert((*element_0).simplex_id() == (*element_1).simplex_id());
 
         // get the flipped segments
         auto segment_a_flip = mito::topology::flip(segment_a);    // vertex1 -> vertex0
@@ -59,12 +60,12 @@ main()
         // ask factory for a triangle with the flipped segments
         auto element_2 =
             mito::topology::triangle({ segment_a_flip, segment_c_flip, segment_b_flip });
-        assert(element_0.simplex_id() != element_2.simplex_id());
+        assert((*element_0).simplex_id() != (*element_2).simplex_id());
 
         // get the flipped triangle
         auto element_0_flip = mito::topology::flip(element_0);
         // assert that the triangle with flipped segments is the same object as the flipped triangle
-        assert(element_0_flip.simplex_id() == element_2.simplex_id());
+        assert((*element_0_flip).simplex_id() == (*element_2).simplex_id());
     }
 
     // all done
