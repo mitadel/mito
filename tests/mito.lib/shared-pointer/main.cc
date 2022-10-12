3b1638f691548aff7b6cf4b9921e86d76630339d
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-10-12 17:52:59 +0200

    tests/mito.lib/shared-pointer: add tests of constructors, destructor, assignment operators

--- tests/mito.lib/shared-pointer/main.cc
+++ tests/mito.lib/shared-pointer/main.cc
@@ -14,6 +14,109 @@ class Resource : public mito::utilities::Shareable {
 // the type of resource to be stored
 using resource_t = Resource;
 
+TEST(SharedPointer, DefaultConstructor)
+{
+    mito::utilities::Shareable shareable1;
+
+    resource_t resource(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr1(&resource);
+
+    EXPECT_EQ(shareable1.references(), shared_ptr1.references());
+}
+
+TEST(SharedPointer, CopyConstructor)
+{
+    resource_t resource(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr1(&resource);
+    mito::utilities::SharedPointer<resource_t> shared_ptr2 = shared_ptr1;
+
+    mito::utilities::Shareable shareable1;
+    mito::utilities::Shareable shareable2 = shareable1;
+
+    EXPECT_EQ(shareable1.references(), shared_ptr1.references());
+    EXPECT_EQ(shareable2.references(), shared_ptr2.references());
+}
+
+TEST(SharedPointer, MoveConstructor)
+{
+    resource_t resource(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr1(&resource);
+    mito::utilities::SharedPointer<resource_t> shared_ptr2(std::move(shared_ptr1));
+
+    mito::utilities::Shareable shareable1;
+    mito::utilities::Shareable shareable2(std::move(shareable1));
+
+    EXPECT_EQ(shareable2.references(), shared_ptr2.references());
+}
+
+TEST(SharedPointer, AssignmentOperator)
+{
+    resource_t resource1(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr1(&resource1);
+    resource_t resource2(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr2(&resource2);
+    shared_ptr2 = shared_ptr1;
+
+    mito::utilities::Shareable shareable1;
+    mito::utilities::Shareable shareable2;
+    shareable2 = shareable1;
+
+    EXPECT_EQ(shareable1.references(), shared_ptr1.references());
+    EXPECT_EQ(shareable2.references(), shared_ptr2.references());
+}
+
+TEST(SharedPointer, MoveAssignmentOperator)
+{
+    resource_t resource1(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr1(&resource1);
+    resource_t resource2(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr2(&resource2);
+    shared_ptr2 = std::move(shared_ptr1);
+
+    mito::utilities::Shareable shareable1;
+    mito::utilities::Shareable shareable2;
+    shareable2 = std::move(shareable1);
+
+    EXPECT_EQ(shareable2.references(), shared_ptr2.references());
+}
+
+TEST(SharedPointer, ThreeWayAssignment)
+{
+    resource_t resource1(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr1(&resource1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr2 = shared_ptr1;
+    resource_t resource3(1);
+    mito::utilities::SharedPointer<resource_t> shared_ptr3(&resource3);
+    shared_ptr2 = shared_ptr3;
+
+    mito::utilities::Shareable shareable1;
+    mito::utilities::Shareable shareable2 = shareable1;
+    mito::utilities::Shareable shareable3;
+    shareable2 = shareable3;
+
+    EXPECT_EQ(shareable1.references(), shared_ptr1.references());
+    EXPECT_EQ(shareable2.references(), shared_ptr2.references());
+    EXPECT_EQ(shareable3.references(), shared_ptr3.references());
+}
+
+TEST(SharedPointer, Destructor)
+{
+    mito::utilities::SharedPointer<resource_t> shared_ptr2;
+    {
+        resource_t resource1(1);
+        mito::utilities::SharedPointer<resource_t> shared_ptr1(&resource1);
+        shared_ptr2 = shared_ptr1;
+    }
+
+    mito::utilities::Shareable shareable2;
+    {
+        mito::utilities::Shareable shareable1;
+        shareable2 = shareable1;
+    }
+
+    EXPECT_EQ(shareable2.references(), shared_ptr2.references());
+}
+
 TEST(SharedPointer, TestSegmentAllocation)
 {
     // the size of the segment of memory
