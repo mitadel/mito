7b4780c66a7b7da441989933da64ef2481f19f2e
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-10-12 17:52:43 +0200

    tests/mito.lib/shared-pointer: minor simplification of test case

--- tests/mito.lib/shared-pointer/main.cc
+++ tests/mito.lib/shared-pointer/main.cc
@@ -4,20 +4,18 @@
 
 class Resource : public mito::utilities::Shareable {
   public:
-    Resource(int a, Resource * c) : _a(a)
-    {
-        std::cout << "Built resource " << _a << " " << c << std::endl;
-    }
+    Resource(int a) : _a(a) { std::cout << "Built resource " << _a << std::endl; }
 
     ~Resource() { std::cout << "Destroying resource " << _a << std::endl; }
 
     int _a;
 };
 
-TEST(SharedPointer, TestSharedPointer)
+// the type of resource to be stored
+using resource_t = Resource;
+
+TEST(SharedPointer, TestSegmentAllocation)
 {
-    // the type of resource to be stored
-    using resource_t = Resource;
     // the size of the segment of memory
     constexpr int N = 10;
     // the initialization value for the resource
@@ -30,7 +28,7 @@ TEST(SharedPointer, TestSharedPointer)
     resource_t * location = (segment + 3);
 
     // instantiate new resource at {location}
-    resource_t * resource = new (location) resource_t(a, nullptr);
+    resource_t * resource = new (location) resource_t(a);
     mito::utilities::shared_ptr<resource_t> handle(resource);
 
     // modify the resource
