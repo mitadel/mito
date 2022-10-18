ca79cd9158de2b03b53d8296cea15fef2a0e7d7e
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-10-18 17:21:59 +0200

    lib/mito/utilities: temporarily remove {requires} clause in {SharedPointer}
    This clause requires that the resource pointed to is 'reference countable'. However, because the compiler needs to see the header file to evaluate the constraint, there is a circular reference of header files that is not easy to solve. This will be addressed in a separate issue.
    
    Co-authored-by: Anwar Koshakji <anwar.koshakji@gmail.com>

--- lib/mito/utilities/Shareable.h
+++ lib/mito/utilities/Shareable.h
@@ -55,7 +55,7 @@ namespace mito::utilities {
         // friendship with SharedPointer (the shared pointer needs r/w access to the reference count
         // of the Shareable instance)
         template <class T>
-        requires mito::utilities::ReferenceCountedObject<T>
+        // requires mito::utilities::ReferenceCountedObject<T>
         friend class mito::utilities::SharedPointer;
     };
 }
