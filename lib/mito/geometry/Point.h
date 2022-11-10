c0651dc45f2dce05c8ceae5f5fa2fd6258a4b50f
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-11-10 20:55:14 +0100

    lib/mito/geometry: minor redesign of {Point}
    Add attribute with coordinates.

--- lib/mito/geometry/Point.h
+++ lib/mito/geometry/Point.h
@@ -6,18 +6,21 @@
 namespace mito::geometry {
 
     template <int D>
-    class Point : public vector_t<D>, public mito::utilities::Shareable {
+    class Point : public mito::utilities::Shareable {
       public:    // TOFIX: should be private (do not allow to instantiate points outside of a point
                  // cloud)
         template <class... Args>
         Point(Args &&... args)
         requires(sizeof...(Args) == D)
-            : vector_t<D>(args...), mito::utilities::Shareable()
+            : _coodinates(args...)
         {}
 
       public:
         // get the coordinates of the point
-        auto coordinates() -> vector_t<D> { return static_cast<vector_t<D>>(*this); }
+        auto coordinates() const -> const vector_t<D> & { return _coodinates; }
+
+      private:
+        vector_t<D> _coodinates;
     };
 
 }    // namespace mito
