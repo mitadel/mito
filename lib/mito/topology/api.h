f009dc5745c589b5abc8c784e3432a33d5ec0fa5
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-11-11 20:44:15 +0100

    lib/mito/topology: fix compile time error
    Leftover declarations of simplices factories.

--- lib/mito/topology/api.h
+++ lib/mito/topology/api.h
@@ -5,18 +5,6 @@
 
 namespace mito::topology {
 
-    // vertex factory
-    inline auto vertex() -> simplex_t<0>;
-
-    // segment factory
-    inline auto segment(const simplex_composition_t<1> & simplices) -> simplex_t<1>;
-
-    // triangle factory
-    inline auto triangle(const simplex_composition_t<2> & simplices) -> simplex_t<2>;
-
-    // tetrahedron factory
-    inline auto tetrahedron(const simplex_composition_t<3> & simplices) -> simplex_t<3>;
-
     template <int D>
     inline auto exists_flipped(const simplex_t<D> & oriented_simplex) -> auto;
 
