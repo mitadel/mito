7f9220c96595ce309c1b56036a540fca4cadedd2
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2022-12-04 19:03:11 +0100

    tests/mito.lib: move quadrature on loaded mesh test to {quadrature} test directory

--- tests/mito.lib/mesh/main.cc
+++ tests/mito.lib/mesh/main.cc
@@ -115,34 +115,3 @@ TEST(Mesh, LoadSummitMesh3D)
     auto mesh = mito::mesh::summit<3, mito::topology::simplex_t>(fileStream, topology, point_cloud);
     std::cout << "Loaded mesh in " << clock() - t << std::endl;
 }
-
-TEST(Mesh, QuadratureOnMesh)
-{
-    // an empty topology
-    auto & topology = mito::topology::topology();
-
-    // an empty cloud of points
-    auto point_cloud = mito::geometry::point_cloud<2>();
-
-    // load mesh
-    std::ifstream fileStream("square.summit");
-    auto mesh = mito::mesh::summit<2, mito::topology::simplex_t>(fileStream, topology, point_cloud);
-    // TOFIX: is it better that the cells and vertices are first fetched from mesh and then
-    //  used to build the manifold?
-    // instantiate a cell set as a collection of simplices and vertices.
-    // const auto & cells = mesh.cells<2>();    // TODO: region label to fetch cells
-    // auto manifold = mito::manifolds::manifold<2>(cells);
-    auto manifold = mito::manifolds::manifold<2>(mesh);
-
-    // instantiate a scalar field
-    auto f = mito::math::function([](const mito::vector_t<2> & x) { return cos(x[0] * x[1]); });
-    auto f_cosine = mito::math::field(f);
-
-    // instantiate a GAUSS integrator with degree of exactness equal to 2
-    auto integrator = mito::quadrature::integrator<mito::quadrature::GAUSS, 2>(manifold);
-
-    auto result = integrator.integrate(f_cosine);
-    std::cout << "Integration of cos(x*y): Result = " << result
-              << ", Error = " << std::fabs(result - 0.946083) << std::endl;
-    EXPECT_NEAR(result, 0.946083, 1.e-7);
-}
