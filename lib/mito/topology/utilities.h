a46648a08cfadf784738c07562345820b7488dc6
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-02-22 20:03:32 +0100

    lib/mito/topology/utilities: add return value to {head} and {tail} free functions

--- lib/mito/topology/utilities.h
+++ lib/mito/topology/utilities.h
@@ -16,7 +16,7 @@ namespace mito::topology {
         return os;
     }
 
-    auto tail(const simplex_t<1> & oriented_simplex)
+    auto tail(const simplex_t<1> & oriented_simplex) -> vertex_t
     {
         if (oriented_simplex->orientation()) {
             return oriented_simplex->composition()[0]->footprint();
@@ -25,7 +25,7 @@ namespace mito::topology {
         }
     }
 
-    auto head(const simplex_t<1> & oriented_simplex)
+    auto head(const simplex_t<1> & oriented_simplex) -> vertex_t
     {
         if (oriented_simplex->orientation()) {
             return oriented_simplex->composition()[1]->footprint();
