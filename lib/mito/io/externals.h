b8e933fa5376e950997773f0036248e77c86a1ca
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-05-12 15:32:21 +0200

    lib/mito/mito.h: rearrange includes in alphabetical order

---
+++ lib/mito/io/externals.h
@@ -0,0 +1,15 @@
+// code guard
+#if !defined(mito_io_externals_h)
+#define mito_io_externals_h
+
+// externals
+#include <unordered_map>
+
+// support
+#include "../geometry.h"
+#include "../mesh.h"
+#include "../topology.h"
+
+#endif
+
+// end of file
---
+++ lib/mito/io/public.h
@@ -0,0 +1,16 @@
+// code guard
+#if !defined(mito_io_public_h)
+#define mito_io_public_h
+
+
+// external packages
+#include "externals.h"
+
+// classes implementation
+#include "readers/summit.h"
+#include "writers/vtk.h"
+
+
+#endif
+
+// end of file
