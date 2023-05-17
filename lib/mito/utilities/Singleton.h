1a58f5ab52b7f6e5ff4736947d45696a726e3096
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-05-17 17:05:48 +0200

    lib/mito/utilities/Singleton.h: remove commented code

--- lib/mito/utilities/Singleton.h
+++ lib/mito/utilities/Singleton.h
@@ -5,49 +5,6 @@
 
 namespace mito::utilities {
 
-    // template <class RESOURCE_T>
-    // class Singleton {
-
-    //     // my template parameter
-    //     using resource_t = RESOURCE_T;
-
-    //   public:
-    //     template <class... Args>
-    //     static auto GetInstance(Args &&... args) -> resource_t &
-    //     {
-    //         if (!resource) {
-    //             resource = new resource_t(args...);
-    //             return *resource;
-    //         }
-
-    //         return *resource;
-    //     }
-
-    //     static auto DeleteInstance() -> void
-    //     {
-    //         delete resource;
-    //         resource = nullptr;
-    //     }
-
-    //   protected:
-    //     Singleton()
-    //     {
-    //         std::cout << "Singleton constructor" << std::endl;
-    //         return;
-    //     }
-    //     ~Singleton() { std::cout << "Singleton destructor" << std::endl; }
-    //     Singleton(const Singleton &) = delete;
-    //     Singleton & operator=(const Singleton &) = delete;
-
-    //   private:
-    //     // the singleton
-    //     static resource_t * resource;
-    // };
-
-    // // initialization of static member
-    // template <class RESOURCE_T>
-    // RESOURCE_T * utilities::Singleton<RESOURCE_T>::resource = nullptr;
-
     template <class RESOURCE_T>
     class Singleton {
 
@@ -67,12 +24,8 @@ namespace mito::utilities {
         static auto DestroyInstance() -> void { resource.reset(); }
 
       protected:
-        Singleton()
-        {
-            std::cout << "Singleton constructor" << std::endl;
-            return;
-        }
-        virtual ~Singleton() { std::cout << "Singleton destructor" << std::endl; }
+        Singleton() {}
+        virtual ~Singleton() {}
 
         Singleton(const Singleton &) = delete;
         Singleton & operator=(const Singleton &) = delete;
