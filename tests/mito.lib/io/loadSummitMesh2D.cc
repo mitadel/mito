46641a4cd5e4fca14ae3c7b6f13fb36be8e9b65c
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-05-22 15:36:52 +0200

    tests/mito.lib/io: separate tests into different files
    This prevents singletons to be reused among different tests.

---
+++ tests/mito.lib/io/loadSummitMesh2D.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <mito/mito.h>
+
+
+TEST(MeshReader, LoadSummitMesh2D)
+{
+    // an empty topology
+    auto & topology = mito::topology::topology();
+
+    // an empty cloud of points
+    auto & point_cloud = mito::geometry::point_cloud<2>();
+
+    // a 2D geometry binding the topology {topology} on the cloud of points {point_cloud}
+    auto & geometry = mito::geometry::geometry(topology, point_cloud);
+
+    clock_t t;
+
+    //
+    t = clock();
+    std::ifstream fileStream("rectangle.summit");
+    auto mesh = mito::io::mesh::reader<mito::topology::simplex_t<2>>(fileStream, geometry);
+    std::cout << "Loaded mesh in " << clock() - t << std::endl;
+
+    t = clock();
+    auto boundary_mesh = mesh.boundary();
+    std::cout << "Fetched boundary in " << clock() - t << std::endl;
+}
