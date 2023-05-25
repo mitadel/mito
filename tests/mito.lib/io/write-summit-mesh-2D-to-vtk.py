2a6343c9b9b2d2b9b81175812fbf46dc36e3c1c7
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-05-25 17:28:20 +0200

    tests/mito.lib/io: add test case for writing a 2D mesh in {vtk} format

---
+++ tests/mito.lib/io/write-summit-mesh-2D-to-vtk.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+import vtk
+
+
+def load_mesh(filename):
+    # get file path
+    test_directory = os.path.dirname(os.path.abspath(__file__))
+    filepath = test_directory + "/" + filename
+    print("File path:", filepath)
+
+    # load the VTK file
+    reader = vtk.vtkXMLUnstructuredGridReader()
+    reader.SetFileName(filepath)
+    reader.Update()
+    return reader.GetOutput()
+
+
+def test_SummitMeshToVtk():
+    filename = "rectangle_output.vtk"
+
+    # get the loaded mesh
+    mesh = load_mesh(filename)
+
+    # check the number of cells (tetrahedra)
+    num_cells = mesh.GetNumberOfCells()
+    print("Number of cells:", num_cells)
+    assert num_cells == 3690
+
+    # check points of the mesh
+    points = mesh.GetPoints()
+    num_points = points.GetNumberOfPoints()
+    print("Number of points:", num_points)
+    assert num_points == 1930
+
+    # cleanup
+    os.remove(filename)
