9c1419596015deeb48444cee16d0f4c3ecd2a781
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-05-26 17:04:08 +0200

    tests/mito.ext: rename file name for integration in 2D

--- .cmake/mito_tests_mito_ext.cmake
+++ .cmake/mito_tests_mito_ext.cmake
@@ -1,4 +1,4 @@
-mito_test_python_testcase(tests/mito.ext/integrate.py)
+mito_test_python_testcase(tests/mito.ext/integrate-2D.py)
 mito_test_python_testcase(tests/mito.ext/scalar-field-2D.py)
 mito_test_python_testcase(tests/mito.ext/scalar-field-3D.py)
 mito_test_python_testcase(tests/mito.ext/vector2D.py)
