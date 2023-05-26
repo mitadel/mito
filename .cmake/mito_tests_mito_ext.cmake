1650506ee179be6ebf7fbe1df7dfd49346fd4833
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-05-26 17:02:02 +0200

    tests/mito.ext: add tests for 2D and 3D scalar fields

--- .cmake/mito_tests_mito_ext.cmake
+++ .cmake/mito_tests_mito_ext.cmake
@@ -1,3 +1,5 @@
 mito_test_python_testcase(tests/mito.ext/integrate.py)
+mito_test_python_testcase(tests/mito.ext/scalar-field-2D.py)
+mito_test_python_testcase(tests/mito.ext/scalar-field-3D.py)
 mito_test_python_testcase(tests/mito.ext/vector2D.py)
 mito_test_python_testcase(tests/mito.ext/vector3D.py)
---
+++ tests/mito.ext/scalar-field-2D.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import mito
+from numpy import cos
+
+
+def myfunction(x):
+    return eval('cos(x[0] * x[1])')
+
+
+field = mito.ScalarField2D(myfunction)
+
+result = field(mito.Vector2D((2., 1.)))
+assert abs(result - (-0.416146)) < 1.e-3
+
+print("all done!")
