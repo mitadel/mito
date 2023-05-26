667dcb4d7e7e7e940b1ff8a8bf202ba8729d363c
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-05-26 09:16:12 +0200

    tests/mito.lib/io: add python script with helper functions

--- tests/mito.lib/io/write-summit-mesh-3D-to-vtk.py
+++ tests/mito.lib/io/write-summit-mesh-3D-to-vtk.py
@@ -1,26 +1,13 @@
-import os
 import pytest
-import vtk
 
-
-def load_mesh(filename):
-    # get file path
-    test_directory = os.path.dirname(os.path.abspath(__file__))
-    filepath = test_directory + "/" + filename
-    print("File path:", filepath)
-
-    # load the VTK file
-    reader = vtk.vtkXMLUnstructuredGridReader()
-    reader.SetFileName(filepath)
-    reader.Update()
-    return reader.GetOutput()
+from helper import load_vtk_mesh, cleanup
 
 
 def test_SummitMeshToVtk():
     filename = "cube_output.vtk"
 
     # get the loaded mesh
-    mesh = load_mesh(filename)
+    mesh = load_vtk_mesh(filename)
 
     # check the number of cells (tetrahedra)
     num_cells = mesh.GetNumberOfCells()
@@ -33,5 +20,5 @@ def test_SummitMeshToVtk():
     print("Number of points:", num_points)
     assert num_points == 139
 
-    # cleanup
-    os.remove(filename)
+    # remove file
+    cleanup(filename)
