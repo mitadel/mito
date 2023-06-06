95aa1131bac5bc64b60462b9f7fe4612cc184d1b
Anwar Koshakji <anwar.koshakji@gmail.com>
2023-06-06 07:49:37 +0200

    lib/mito/topology: separate {Topology} implementation in {.icc} file

--- lib/mito/topology/Topology.h
+++ lib/mito/topology/Topology.h
@@ -12,11 +12,7 @@ namespace mito::topology {
     class Topology {
       private:
         // default constructor
-        Topology() :
-            _vertex_factory(),
-            _segment_factory(),
-            _triangle_factory(),
-            _tetrahedron_factory() {};
+        Topology();
 
         // delete copy constructor
         Topology(const Topology &) = delete;
@@ -25,150 +21,68 @@ namespace mito::topology {
         void operator=(const Topology &) = delete;
 
         // destructor
-        ~Topology() {};
+        ~Topology();
 
       public:
         template <int D>
         inline auto simplex(const unoriented_simplex_t<D> & footprint, bool orientation)
-            -> const simplex_t<D> &
-        {
-            // ask the factory of oriented simplices
-            return _get_factory<D>().orientedSimplex(footprint, orientation);
-        }
+            -> const simplex_t<D> &;
 
         // return a simplex with composition {composition} (either create a new simplex if such
         // simplex does not exist in the factory or return the existing representative of the class
         // of equivalence of simplices with this composition)
         template <int D>
         inline auto simplex(const simplex_composition_t<D> & composition) -> const simplex_t<D> &
-        requires(D > 0)
-        {
-            // ask the factory of oriented simplices
-            return _get_factory<D>().orientedSimplex(composition);
-        }
+        requires(D > 0);
 
         template <int D>
         inline auto simplex(bool orientation) -> const simplex_t<0> &
-        requires(D == 0)
-        {
-            // ask the factory of oriented simplices
-            return _get_factory<0>().orientedSimplex(orientation);
-        }
+        requires(D == 0);
 
         // instantiate a vertex
         inline auto vertex() -> const vertex_t &;
 
         // instantiate a segment
-        inline auto segment(const simplex_composition_t<1> & simplices) -> const simplex_t<1> &
-        {
-            return simplex<1>(simplices);
-        }
+        inline auto segment(const simplex_composition_t<1> & simplices) -> const simplex_t<1> &;
 
         // instantiate a triangle
-        inline auto triangle(const simplex_composition_t<2> & simplices) -> const simplex_t<2> &
-        {
-            return simplex<2>(simplices);
-        }
+        inline auto triangle(const simplex_composition_t<2> & simplices) -> const simplex_t<2> &;
 
         // instantiate a tetrahedron
-        inline auto tetrahedron(const simplex_composition_t<3> & simplices) -> const simplex_t<3> &
-        {
-            return simplex<3>(simplices);
-        }
+        inline auto tetrahedron(const simplex_composition_t<3> & simplices) -> const simplex_t<3> &;
 
         // instantiate a segment from unoriented vertices
         inline auto segment(const vertex_simplex_composition_t<1> & simplices)
-            -> const simplex_t<1> &
-        {
-            return segment({ simplex(simplices[0], false), simplex(simplices[1], true) });
-        }
+            -> const simplex_t<1> &;
 
         // instantiate a triangle
         inline auto triangle(const vertex_simplex_composition_t<2> & vertices)
-            -> const simplex_t<2> &
-        {
-            return simplex<2>({ segment({ vertices[0], vertices[1] }),
-                                segment({ vertices[1], vertices[2] }),
-                                segment({ vertices[2], vertices[0] }) });
-        }
+            -> const simplex_t<2> &;
 
         // instantiate a tetrahedron
         inline auto tetrahedron(const vertex_simplex_composition_t<3> & vertices)
-            -> const simplex_t<3> &
-        {
-            return simplex<3>({ triangle({ vertices[0], vertices[1], vertices[3] }),
-                                triangle({ vertices[1], vertices[2], vertices[3] }),
-                                triangle({ vertices[2], vertices[0], vertices[3] }),
-                                triangle({ vertices[0], vertices[2], vertices[1] }) });
-        }
+            -> const simplex_t<3> &;
 
         // returns whether the oriented simplex exists in the factory
         template <int D>
-        inline auto exists(const simplex_t<D> & simplex) const -> bool
-        {
-            return _get_factory<D>().existsOrientedSimplex(
-                simplex->footprint(), simplex->orientation());
-        }
+        inline auto exists(const simplex_t<D> & simplex) const -> bool;
 
         // returns whether there exists the flipped oriented simplex in the factory
         template <int D>
-        inline auto exists_flipped(const simplex_t<D> & simplex) const -> bool
-        {
-            return _get_factory<D>().existsOrientedSimplex(
-                simplex->footprint(), !simplex->orientation());
-        }
+        inline auto exists_flipped(const simplex_t<D> & simplex) const -> bool;
 
         // returns the simplex with opposite orientation
         template <int D>
-        inline auto flip(const simplex_t<D> & simplex) -> const simplex_t<D> &
-        {
-            // ask the factory of oriented simplices
-            return _get_factory<D>().orientedSimplex(simplex->footprint(), !simplex->orientation());
-        }
+        inline auto flip(const simplex_t<D> & simplex) -> const simplex_t<D> &;
 
       private:
         template <int D>
         inline auto _erase(const simplex_t<D> & simplex) -> void
-        requires(D == 0)
-        {
-            // erase the vertex from the factory
-            _get_factory<D>().erase(simplex);
-
-            // all done
-            return;
-        }
+        requires(D == 0);
 
         template <int D>
         inline auto _erase(const simplex_t<D> & simplex) -> void
-        requires(D > 0)
-        {
-            // QUESTION: method {reset} is not {const} unless the {_handle} of the shared pointer is
-            //          declared mutable. Should we call reset here and make the handle mutable or
-            //          should we accept that {element} points to an invalid resource after call to
-            //          {erase}?
-
-            // sanity check
-            assert(simplex.references() > 0);
-
-            // grab a copy of the composition
-            auto composition = simplex->composition();
-
-            // cleanup the oriented factory around {simplex}
-            _get_factory<D>().erase(simplex);
-
-            // loop on subsimplices
-            for (const auto & subsimplex : composition) {
-                // if this simplex is the last one using the subsimplex (other than the copy we just
-                // did)
-                if (subsimplex.references() == 2) {
-                    // recursively erase the subsimplices
-                    _erase(subsimplex);
-                }
-            }
-
-            // all done
-            return;
-        }
+        requires(D > 0);
 
         // mutator for the simplex factory of dimension D
         template <int D>
@@ -180,17 +94,7 @@ namespace mito::topology {
 
       public:
         template <int D>
-        inline auto erase(const simplex_t<D> & simplex) -> void
-        {
-            // if someone else (other than the topology) is still using this resource
-            if (simplex.references() > 1) {
-                // do nothing
-                return;
-            }
-
-            // otherwise, erase the simplex
-            return _erase(simplex);
-        }
+        inline auto erase(const simplex_t<D> & simplex) -> void;
 
       private:
         // factory for vertices
@@ -211,71 +115,11 @@ namespace mito::topology {
     };
 }
 
-// TOFIX: split header into {Topology.h} and {Topology.icc} so these can be moved to the icc file
-template <>
-inline auto
-mito::topology::Topology::_get_factory<0>() -> oriented_simplex_factory_t<0> &
-{
-    return _vertex_factory;
-}
 
-template <>
-inline auto
-mito::topology::Topology::_get_factory<1>() -> oriented_simplex_factory_t<1> &
-{
-    return _segment_factory;
-}
-
-template <>
-inline auto
-mito::topology::Topology::_get_factory<2>() -> oriented_simplex_factory_t<2> &
-{
-    return _triangle_factory;
-}
-
-template <>
-inline auto
-mito::topology::Topology::_get_factory<3>() -> oriented_simplex_factory_t<3> &
-{
-    return _tetrahedron_factory;
-}
-
-template <>
-inline auto
-mito::topology::Topology::_get_factory<0>() const -> const oriented_simplex_factory_t<0> &
-{
-    return _vertex_factory;
-}
-
-template <>
-inline auto
-mito::topology::Topology::_get_factory<1>() const -> const oriented_simplex_factory_t<1> &
-{
-    return _segment_factory;
-}
-
-template <>
-inline auto
-mito::topology::Topology::_get_factory<2>() const -> const oriented_simplex_factory_t<2> &
-{
-    return _triangle_factory;
-}
-
-template <>
-inline auto
-mito::topology::Topology::_get_factory<3>() const -> const oriented_simplex_factory_t<3> &
-{
-    return _tetrahedron_factory;
-}
-
-// instantiate a vertex
-inline auto
-mito::topology::Topology::vertex() -> const vertex_t &
-{
-    // ask the factory of vertices for an unoriented vertex
-    return _get_factory<0>().vertex();
-}
+// get the inline definitions
+#define mito_topology_Topology_icc
+#include "Topology.icc"
+#undef mito_topology_Topology_icc
 
 #endif    // mito_topology_Topology_h
-
 // end of file
