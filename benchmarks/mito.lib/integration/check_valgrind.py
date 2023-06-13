ae1f2f507123e23c564dc641f53575bbd0c92769
Anwar Koshakji <anwk84@gmail.com>
2023-06-13 15:56:32 +0200

    benchmarks/mito.lib/integration: add python script to check valgrind output in xml format

---
+++ benchmarks/mito.lib/integration/check_valgrind.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as ET
+
+# Load the XML file
+tree = ET.parse('./memcheck.xml')
+
+# Get the root element
+root = tree.getroot()
+
+# Check for elements named "error"
+error_elements = [element for element in root.iter() if element.tag == "error"]
+
+# Print the names of the error elements
+if error_elements:
+    raise RuntimeError("Errors found:", len(error_elements), "Please check valgrind output")
+else:
+    print("No errors found.")
