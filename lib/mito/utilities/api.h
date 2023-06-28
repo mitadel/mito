20e276e8ef1d60d6b3f6f660e12ab26c123866b0
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-06-28 18:29:41 +0200

    lib/mito/utilities: move {typedef}'s to api

--- lib/mito/utilities/api.h
+++ lib/mito/utilities/api.h
@@ -5,7 +5,17 @@
 
 namespace mito::utilities {
 
+    // shared pointer alias
+    template <class Resource>
+    using shared_ptr = SharedPointer<Resource>;
 
+    // index type alias
+    template <class Resource>
+    using index_t = std::uintptr_t;
+
+    // segmented container alias
+    template <class T>
+    using segmented_t = SegmentedContainer<typename T::resource_t>;
 }
 
 
