acea6f08f30450e784e95fff071bb2a37b48f05f
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-08-01 08:23:07 +0200

    lib/mito/mesh/metis: move partitioner implementation from test to library

--- lib/mito/mesh/metis/partitioner.h
+++ lib/mito/mesh/metis/partitioner.h
@@ -1,87 +1,4 @@
 
-#include <gtest/gtest.h>
-#include <mito/base.h>
-#include <mito/mesh.h>
-
-using geometry_t = mito::geometry::geometry_t<2>;
-using mesh_t = mito::mesh::mesh_t<mito::topology::triangle_t, 2>;
-using vertex_t = mito::topology::vertex_t;
-using edge_t = mito::topology::segment_t;
-
-auto
-build_mesh(geometry_t & geometry, mesh_t & mesh) -> void
-{
-    /**
-     * Mesh with four cells:
-        (0,1)           (1,1)
-          4               2
-          +---------------+
-          | .           . |
-          |   .       .   |
-          |     .   .     |
-          |       . 3     |
-          |     .   .     |
-          |   .       .   |
-          | .           . |
-          +---------------+
-          0               1
-        (0,0)           (1,0)
-    */
-
-    auto & point_cloud = geometry.point_cloud();
-    auto & topology = geometry.topology();
-
-    auto point0 = point_cloud.point({ 0.0, 0.0 });
-    auto point1 = point_cloud.point({ 1.0, 0.0 });
-    auto point2 = point_cloud.point({ 1.0, 1.0 });
-    auto point3 = point_cloud.point({ 0.5, 0.5 });
-    auto point4 = point_cloud.point({ 0.0, 1.0 });
-
-    auto vertex0 = topology.vertex();
-    auto vertex1 = topology.vertex();
-    auto vertex2 = topology.vertex();
-    auto vertex3 = topology.vertex();
-    auto vertex4 = topology.vertex();
-
-    // print the vertices ids in order of construction
-    // std::cout << vertex0.id() << "\t" << vertex1.id() << "\t" << vertex2.id() << "\t"
-    //           << vertex3.id() << "\t" << vertex4.id() << std::endl;
-
-    // add nodes to geometry
-    geometry.node(vertex0, point0);
-    geometry.node(vertex1, point1);
-    geometry.node(vertex2, point2);
-    geometry.node(vertex3, point3);
-    geometry.node(vertex4, point4);
-
-    auto segment0 = topology.segment({ vertex0, vertex1 });
-    auto segment1 = topology.segment({ vertex1, vertex3 });
-    auto segment2 = topology.segment({ vertex3, vertex0 });
-    auto cell0 = topology.triangle({ segment0, segment1, segment2 });
-
-    auto segment3 = topology.segment({ vertex1, vertex2 });
-    auto segment4 = topology.segment({ vertex2, vertex3 });
-    auto segment5 = topology.segment({ vertex3, vertex1 });
-    auto cell1 = topology.triangle({ segment3, segment4, segment5 });
-
-    auto segment6 = topology.segment({ vertex2, vertex4 });
-    auto segment7 = topology.segment({ vertex4, vertex3 });
-    auto segment8 = topology.segment({ vertex3, vertex2 });
-    auto cell2 = topology.triangle({ segment6, segment7, segment8 });
-
-    auto segment9 = topology.segment({ vertex4, vertex0 });
-    auto segment10 = topology.segment({ vertex0, vertex3 });
-    auto segment11 = topology.segment({ vertex3, vertex4 });
-    auto cell3 = topology.triangle({ segment9, segment10, segment11 });
-
-    mesh.insert(cell0);
-    mesh.insert(cell1);
-    mesh.insert(cell2);
-    mesh.insert(cell3);
-
-    return;
-}
-
 auto
 populate_collection_of_vertices(const mesh_t & mesh) -> std::vector<vertex_t>
 {
@@ -210,9 +127,6 @@ metis_paint_partition(
         &nVertices_metis, &nWeights, xadjacency.data(), adjacency.data(), NULL, NULL, NULL,
         &nPartitions_metis, NULL, NULL, options, &objval, painting.data());
 
-    // assert metis ran correctly
-    EXPECT_EQ(metisResult, 1);
-
     // all done
     return painting;
 }
@@ -222,18 +136,12 @@ partition(const mesh_t & mesh, int nPartitions) -> void
 {
     // a collection of all the vertices
     auto id_to_vertex = populate_collection_of_vertices(mesh);
-    // check that you found 5 vertices
-    EXPECT_EQ(id_to_vertex.size(), 5);
 
     // a map between the vertices and an integer id
     auto vertex_to_id = populate_vertices_map(id_to_vertex);
-    // assert you assigned 5 labels
-    EXPECT_EQ(vertex_to_id.size(), 5);
 
     // a collection of all the edges (without repeated entries)
     auto edge_collection = populate_collection_of_edges(mesh);
-    // check that you found 12 edges
-    EXPECT_EQ(edge_collection.size(), 12);
 
     // std::cout << "vertex map" << std::endl;
     // for (const auto & vertex : vertex_to_id) {
@@ -258,8 +166,8 @@ partition(const mesh_t & mesh, int nPartitions) -> void
     int nVertices = mesh.geometry().point_cloud().size();
 
     // compute the total number of edges (with repeated orientation)
+    // TOFIX: is this general?
     int nEdges = mesh.cells().size() * 3 + mesh.boundary_size();
-    EXPECT_EQ(nEdges, 16);
 
     // call metis partitioner
     auto painting = metis_paint_partition(nVertices, nEdges, nPartitions, adjacency_map);
@@ -268,33 +176,6 @@ partition(const mesh_t & mesh, int nPartitions) -> void
     for (size_t part_i = 0; part_i < painting.size(); part_i++) {
         std::cout << part_i << " " << painting[part_i] << std::endl;
     }
-
-    // all done
-    return;
-}
-
-TEST(MetisPartitioner, Base)
-{
-    // an empty topology
-    auto topology = mito::topology::topology();
-
-    // an empty cloud of points
-    auto point_cloud = mito::geometry::point_cloud<2>();
-
-    // a 2D geometry binding the topology {topology} on the cloud of points {point_cloud}
-    auto geometry = mito::geometry::geometry(topology, point_cloud);
-
-    // an empty mesh of simplicial topology in 2D
-    auto mesh = mito::mesh::mesh<mito::topology::triangle_t>(geometry);
-
-    // populate the mesh
-    build_mesh(geometry, mesh);
-
-    // number of partitions
-    int n_partitions = 2;
-
-    // partition the mesh
-    partition(mesh, n_partitions);
 
     // all done
     return;
