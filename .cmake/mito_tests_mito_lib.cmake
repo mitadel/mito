bf94a996e22c231514171d213be5695034b045e7
Anwar Koshakji <anwk84@gmail.com>
2023-09-18 18:10:33 +0200

    .cmake/mito_tests_mito_lib.cmake: add missing guard for {VTK}

--- .cmake/mito_tests_mito_lib.cmake
+++ .cmake/mito_tests_mito_lib.cmake
@@ -1,4 +1,4 @@
-# geometry
+#  geometry
 mito_test_driver(tests/mito.lib/geometry/cell_barycenter.cc)
 mito_test_driver(tests/mito.lib/geometry/point.cc)
 
@@ -6,6 +6,7 @@ mito_test_driver(tests/mito.lib/geometry/point.cc)
 mito_test_driver(tests/mito.lib/io/read_mesh_summit_2D.cc)
 mito_test_driver(tests/mito.lib/io/read_mesh_summit_3D.cc)
 mito_test_driver(tests/mito.lib/io/read_mesh_summit_segment_3D.cc)
+
 if(WITH_VTK)
     mito_test_driver_pytest_check(tests/mito.lib/io/write_point_cloud_vtk.cc)
     mito_test_driver_pytest_check(tests/mito.lib/io/summit_to_vtk_mesh_3D.cc)
@@ -27,10 +28,12 @@ mito_test_driver(tests/mito.lib/mesh/tetra_3D.cc)
 mito_test_driver(tests/mito.lib/mesh/tetra_zero_subdivisions.cc)
 mito_test_driver(tests/mito.lib/mesh/tetra_multiple_subdivisions.cc)
 mito_test_driver(tests/mito.lib/mesh/erase_element.cc)
+
 if(WITH_METIS)
     mito_test_driver(tests/mito.lib/mesh/metis_partitioner.cc)
     mito_test_driver(tests/mito.lib/mesh/metis_partitioner_load_mesh.cc)
     mito_test_driver(tests/mito.lib/mesh/metis_partitioner_single_partition.cc)
+
     if(WITH_MPI)
         mito_test_driver_mpi(tests/mito.lib/mesh/metis_partitioner_mpi.cc 2)
         mito_test_driver_mpi(tests/mito.lib/mesh/metis_partitioner_mpi_load_mesh.cc 2)
@@ -61,7 +64,9 @@ if(${WITH_METIS} AND ${WITH_MPI})
     mito_test_driver_mpi(tests/mito.lib/integration/quadrature_load_mesh_2D_mpi.cc 2)
 endif()
 
-mito_test_driver_pytest_check(tests/mito.lib/integration/write_tetra_mesh_to_vtk.cc)
+if(${WITH_VTK})
+    mito_test_driver_pytest_check(tests/mito.lib/integration/write_tetra_mesh_to_vtk.cc)
+endif()
 
 # unsorted
 mito_test_driver(tests/pyre-grids/main.cc)
