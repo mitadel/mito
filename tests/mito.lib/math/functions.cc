bb6c6fcd493205d6ab1bde9f15e131d88aa23b20
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-10-01 08:58:07 +0200

    tests/mito.lib/math: test of functions is now {static_assert}'ed

--- tests/mito.lib/math/functions.cc
+++ tests/mito.lib/math/functions.cc
@@ -3,9 +3,7 @@
 #include <mito/math.h>
 
 
-using mito::math::function_t;
-
-static const mito::real PI = 4.0 * atan(1.0);
+static constexpr mito::real PI = 4.0 * atan(1.0);
 
 //
 // mito::real
@@ -19,47 +17,47 @@ my_function(const mito::vector_t<2> & x)
 TEST(Functions, Sanity)
 {
     // a scalar function
-    auto function1 = mito::math::function(
+    constexpr auto function1 = mito::math::function(
         [](const mito::vector_t<2> & x) -> mito::scalar_t { return cos(x[0] * x[1]); });
 
     // a scalar function
-    auto function2 =
+    constexpr auto function2 =
         mito::math::function([](const mito::vector_t<2> & /*x*/) -> mito::scalar_t { return 5; });
 
     // the sum of the two scalar functions
-    auto function3 = function1 + function2;
+    constexpr auto function3 = function1 + function2;
 
     // a point in the reference configuration
-    mito::vector_t<2> x = { 1.0, PI };
+    constexpr mito::vector_t<2> x = { 1.0, PI };
 
     std::cout << "x = " << x << std::endl;
     std::cout << "f1 = cos(x[0]*x[1]), f1(x) = " << function1(x) << std::endl;
     std::cout << "f2 = 5 , f2(x) = " << function2(x) << std::endl;
     std::cout << "f1(x) + f2(x) = " << function3(x) << std::endl;
 
-    auto function4 = function3 + function3 + function3;
-    auto function5 = 3.0 * function3;
-    EXPECT_DOUBLE_EQ(function4(x), function5(x));
+    constexpr auto function4 = function3 + function3 + function3;
+    constexpr auto function5 = 3.0 * function3;
+    static_assert(function4(x) == function5(x));
 
-    auto function6 = function4 - function3;
-    auto function7 = function5 - function3;
-    EXPECT_DOUBLE_EQ(function6(x), function7(x));
+    constexpr auto function6 = function4 - function3;
+    constexpr auto function7 = function5 - function3;
+    static_assert(function6(x) == function7(x));
 
-    auto function8 = (function7 * 0.5) / 0.5;
-    EXPECT_DOUBLE_EQ(function7(x), function8(x));
+    constexpr auto function8 = (function7 * 0.5) / 0.5;
+    static_assert(function7(x) == function8(x));
 
-    auto function9 = (0.5 / function7) * function7;
-    EXPECT_DOUBLE_EQ(function9(x), 0.5);
+    constexpr auto function9 = (0.5 / function7) * function7;
+    static_assert(function9(x) == 0.5);
 
-    auto function10 = (PI + function7) - function7;
-    EXPECT_DOUBLE_EQ(function10(x), PI);
+    constexpr auto function10 = (PI + function7) - function7;
+    static_assert(function10(x) == PI);
 
-    auto function11 = PI + function7 - 2.0 * PI;
-    EXPECT_DOUBLE_EQ(function11(x), function7(x) - PI);
+    constexpr auto function11 = PI + function7 - 2.0 * PI;
+    static_assert(function11(x) == function7(x) - PI);
 
     // (cos(xy) + 5) / cos(xy)
-    auto function12 = (function1 + function2) / function1;
-    EXPECT_DOUBLE_EQ((function1(x) + function2(x)) / function1(x), function12(x));
+    constexpr auto function12 = (function1 + function2) / function1;
+    static_assert((function1(x) + function2(x)) / function1(x) == function12(x));
 
     // TOFIX: fix the rest of the test
     // auto function13 = mito::math::function(my_function);
