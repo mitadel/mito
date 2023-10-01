a5555dda008918c3d2721e4094d41e4bba2d25ff
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-10-01 21:22:12 +0200

    lib/mito/math: narrative improvements

--- lib/mito/math/Function.h
+++ lib/mito/math/Function.h
@@ -6,22 +6,36 @@
 
 namespace mito::math {
 
+    // This class provides a single interface to lambda functions, {std::function}, and pointers to
+    // functions. The class template parameter is the type of the function wrapped.
+
     template <class F>
     class Function {
-
       private:
+        // type traits on function type
         using traits = std::conditional<
-            std::is_pointer<F>::value, pointer_function_traits<F>, lambda_traits<F>>::type;
+            // if {F} is a pointer to function
+            std::is_pointer<F>::value,
+            // use the traits for pointer to functions
+            pointer_function_traits<F>,
+            // otherwise use the traits for a lambda (these traits also work on {std::function})
+            lambda_traits<F>>::type;
 
       public:
+        // the input type
         using X = typename traits::argument_type;
+        // the output type
         using Y = typename traits::result_type;
 
       public:
+        // constructor
         constexpr Function(F f) : _f { f } {}
+
+        // call operator
         constexpr auto operator()(X x) const -> Y { return _f(x); }
 
       private:
+        // the function wrapped
         F _f;
     };
 }
