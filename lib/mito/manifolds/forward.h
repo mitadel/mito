e3abf3b937aeedbac55e01823816923bea8a4c8c
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-10-07 08:26:50 +0200

    lib/mito/manifolds: move {concept} of one-forms to forms.h

--- lib/mito/manifolds/forward.h
+++ lib/mito/manifolds/forward.h
@@ -13,17 +13,6 @@ namespace mito::manifolds {
     template <class cellT, int D>
     using manifold_t = Manifold<cellT, D>;
 
-    // concept for a function {F} being a one forms on a D-dimensional manifold
-    template <int D, class F>
-    // {F} is a one form if:
-    concept is_one_form =
-        // ... it takes in input a mito::vector_t<D>
-        std::is_same<
-            mito::utilities::base_type<typename mito::math::function_t<F>::X>,
-            mito::vector_t<D>>::value
-        // ... and returns a scalar
-        && std::is_same<typename mito::math::function_t<F>::Y, mito::scalar_t>::value;
-
     // TOFIX: remove this datastructure
     // element vector alias
     template <class elementT>
