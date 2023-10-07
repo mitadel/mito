6997e13b133a0e2b575c341ee14eb9e8db890035
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-10-07 09:07:08 +0200

    lib/mito/manifolds: evaluate {concept} of one-forms on {function_t<F>} instead of {F}

--- lib/mito/manifolds/forms.h
+++ lib/mito/manifolds/forms.h
@@ -17,25 +17,24 @@ namespace mito::manifolds {
     }
 
     // concept for a function {F} being a one forms on a D-dimensional manifold
-    template <int D, class F>
+    template <int D, class FUNCTION>
     // {F} is a one form if:
     concept is_one_form =
         // ... it takes in input a mito::vector_t<D>
-        std::is_same<
-            mito::utilities::base_type<typename mito::math::function_t<F>::X>,
-            mito::vector_t<D>>::value
+        std::is_same<mito::utilities::base_type<typename FUNCTION::X>, mito::vector_t<D>>::value
         // ... and returns a scalar
-        && std::is_same<typename mito::math::function_t<F>::Y, mito::scalar_t>::value;
+        && std::is_same<typename FUNCTION::Y, mito::scalar_t>::value;
 
     // TODO: deduce template parameter {D} based on {a_tilda} and {b_tilda}
     // the wedge product of two forms
     template <int D, class F1, class F2>
-    constexpr auto
-    wedge(const mito::math::function_t<F1> & a_tilda, const mito::math::function_t<F2> & b_tilda)
-    requires (is_one_form<D, F1> && is_one_form<D, F2>)
+    constexpr auto wedge(
+        const mito::math::function_t<F1> & a_tilda, const mito::math::function_t<F2> & b_tilda)
+    requires(
+        is_one_form<D, mito::math::function_t<F1>> && is_one_form<D, mito::math::function_t<F2>>)
     {
         // return the contraction of a given vector {x} with {vector}
-        return mito::math::function([ a_tilda, b_tilda ](const mito::vector_t<D> & x) -> auto{
+        return mito::math::function([a_tilda, b_tilda](const mito::vector_t<D> & x) -> auto {
             // return the contraction of {vector} with {x}
             return a_tilda(x) * b_tilda - b_tilda(x) * a_tilda;
         });
