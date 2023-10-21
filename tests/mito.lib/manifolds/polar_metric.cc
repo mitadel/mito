bd6046953a44e77ce719fa22cf48528e615c07c0
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-10-21 09:08:43 +0200

    tests/mito.lib/manifolds: use inverse metric in definition of one-forms

--- tests/mito.lib/manifolds/polar_metric.cc
+++ tests/mito.lib/manifolds/polar_metric.cc
@@ -5,21 +5,31 @@
 
 TEST(Metric, Polar)
 {
-    // a function from the manifold to the symmetric tensors space
-    // TOFIX: this function is the inverse metric, while it should be the metric
+    // the metric field
     constexpr auto g =
         // x[0] -> r
         // x[1] -> theta
-        mito::manifolds::field([](const mito::vector_t<2> & x) -> mito::diagonal_matrix_t<2> {
-            // e_r + r^2 * e_theta
-            return mito::e_00<2> + 1.0 / (x[0] * x[0]) * mito::e_11<2>;
-        });
+        mito::manifolds::field(
+            [](const mito::geometry::coordinates_t<2> & x) -> mito::diagonal_matrix_t<2> {
+                // e_r + r^2 * e_theta
+                return mito::e_00<2> + (x[0] * x[0]) * mito::e_11<2>;
+            });
+
+    // the inverse metric field
+    constexpr auto g_inv =
+        // x[0] -> r
+        // x[1] -> theta
+        mito::manifolds::field(
+            [](const mito::geometry::coordinates_t<2> & x) -> mito::diagonal_matrix_t<2> {
+                // e_r + 1/r^2 * e_theta
+                return mito::e_00<2> + 1.0 / (x[0] * x[0]) * mito::e_11<2>;
+            });
 
     // the basis one forms
     constexpr auto dr =
-        mito::manifolds::one_form(mito::manifolds::uniform_field<2>(mito::e_0<2>), g);
+        mito::manifolds::one_form(mito::manifolds::uniform_field<2>(mito::e_0<2>), g_inv);
     constexpr auto dtheta =
-        mito::manifolds::one_form(mito::manifolds::uniform_field<2>(mito::e_1<2>), g);
+        mito::manifolds::one_form(mito::manifolds::uniform_field<2>(mito::e_1<2>), g_inv);
 
     // the metric volume element
     constexpr auto w =
