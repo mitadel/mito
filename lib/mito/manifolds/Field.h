639cefc33d98f9f53d9822b4e3ed83a57ac9a32a
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-10-24 18:43:06 +0200

    lib/mito/manifolds: remove unused attributes/typedefs from {Field}

--- lib/mito/manifolds/Field.h
+++ lib/mito/manifolds/Field.h
@@ -7,30 +7,27 @@
 namespace mito::manifolds {
 
     template <class F>
-    // TOFIX: add requirements of taking a position in input and returning a {Tensor} instance
     class Field {
       private:
         static constexpr int D = input<F>::input_dim;
-        using return_type = input<F>::output_type;
 
       public:
         static constexpr int dim = D;
         using function_type = F;
-        static constexpr int rank = input<F>::output_rank;
 
       public:
         // constructor
         constexpr Field(F f) : _f { f } {}
 
         // QUESTION: should this act on a {coordinates_t} or a {point_t}?
-        // the value of the field at {x}
-        constexpr auto operator()(const mito::geometry::coordinates_t<D> & x) const -> return_type
+        // the value of the field at position {x}
+        constexpr auto operator()(const mito::geometry::coordinates_t<D> & x) const
         {
             return _f(x);
         }
 
       private:
-        // the function assigning the value of the tensor field at each point
+        // the function assigning the value of the field at each point
         F _f;
     };
 }
