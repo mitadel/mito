56bf8809d5fd0a0d7159d736a35c6e6e5c538690
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-11-05 16:05:14 +0100

    lib/mito/io: add assert that tetrahedra read from summit mesh have positive volume

--- lib/mito/io/summit/reader.h
+++ lib/mito/io/summit/reader.h
@@ -99,6 +99,24 @@ namespace mito::io::summit {
     }
 
     template <int D>
+    inline auto orientation(
+        const geometry::geometry_t<D> & geometry, const topology::vertex_t & v0,
+        const topology::vertex_t & v1, const topology::vertex_t & v2, const topology::vertex_t & v3)
+        -> bool
+    {
+        auto p0 = geometry.point(v0)->coordinates();
+        auto p1 = geometry.point(v1)->coordinates();
+        auto p2 = geometry.point(v2)->coordinates();
+        auto p3 = geometry.point(v3)->coordinates();
+
+        auto dir1 = p1 - p0;
+        auto dir2 = p2 - p0;
+        auto dir3 = p3 - p0;
+
+        return pyre::tensor::cross(dir1, dir2) * dir3 > 0;
+    }
+
+    template <int D>
     auto readTetrahedron(
         std::ifstream & fileStream, mito::mesh::mesh_t<topology::tetrahedron_t, D> & mesh,
         geometry::geometry_t<D> & geometry, const std::vector<topology::vertex_t> & vertices)
@@ -126,6 +144,8 @@ namespace mito::io::summit {
         const auto & vertex1 = vertices[index1];
         const auto & vertex2 = vertices[index2];
         const auto & vertex3 = vertices[index3];
+
+        assert(orientation(geometry, vertex0, vertex1, vertex2, vertex3));
 
         const auto & cell = topology.tetrahedron({ vertex0, vertex1, vertex2, vertex3 });
         mesh.insert(cell);
