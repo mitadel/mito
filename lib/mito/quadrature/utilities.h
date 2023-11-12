e794d1b5ebcc70a37d968514ec5bc9d6bd4d6630
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-11-12 16:27:36 +0100

    lib/mito/quadrature: remove unnecessary {volume} function in utilities

--- lib/mito/quadrature/utilities.h
+++ lib/mito/quadrature/utilities.h
@@ -5,18 +5,6 @@
 
 namespace mito::quadrature {
 
-    // volume
-    template <class manifoldT>
-    auto volume(const manifoldT & manifold) -> real
-    {
-        // the constant field equal to one
-        auto one = math::field(math::one<vector_t<manifoldT::dim>>());
-        // an integrator on the manifold
-        auto integrator = quadrature::integrator<GAUSS, 1>(manifold);
-        // integrate the constant unit function and return
-        return integrator.integrate(one);
-    }
-
 }
 
 
