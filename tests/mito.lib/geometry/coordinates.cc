3f6fc9a133f5f297cf68b34699b936c7339f2f6e
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-11-26 11:21:42 +0100

    tests/mito.lib/geometry: draft coordinates api

--- tests/mito.lib/geometry/coordinates.cc
+++ tests/mito.lib/geometry/coordinates.cc
@@ -6,18 +6,26 @@
 #include "mito/geometry/Coordinates.h"
 #include "mito/geometry/algebra_coordinates.h"
 
+// coordinates alias in D-dimensions
+template <int D, mito::geometry::CoordinateSystem coordT>
+using coordinates_t = mito::geometry::Coordinates<D, coordT>;
+
+// factory for coordinates from brace-enclosed initializer list
+template <mito::geometry::CoordinateSystem coordT = mito::geometry::EUCLIDEAN, int D>
+constexpr auto
+coordinates(const mito::scalar_t (&&coords)[D])
+{
+    return coordinates_t<D, coordT>(coords);
+}
 
 TEST(Coordinates, Euclidean)
 {
     // create two sets of coordinates
-    constexpr auto coord_A =
-        mito::geometry::Coordinates<2, mito::geometry::EUCLIDEAN>({ 0.0, 1.0 });
-    constexpr auto coord_B =
-        mito::geometry::Coordinates<2, mito::geometry::EUCLIDEAN>({ 1.0, 0.0 });
+    constexpr auto coord_A = coordinates({ 0.0, 1.0 });
+    constexpr auto coord_B = coordinates({ 1.0, 0.0 });
 
     // the exact value of {coord_A + coord_B} / 2
-    constexpr auto coord_C =
-        mito::geometry::Coordinates<2, mito::geometry::EUCLIDEAN>({ 0.5, 0.5 });
+    constexpr auto coord_C = coordinates({ 0.5, 0.5 });
 
     // compute {coord_A + coord_B} / 2.0
     constexpr auto coord_D = (coord_A + coord_B) / 2.0;
@@ -33,13 +41,12 @@ TEST(Coordinates, Euclidean)
 TEST(Coordinates, POLAR)
 {
     // create two sets of coordinates
-    constexpr auto coord_A = mito::geometry::Coordinates<2, mito::geometry::POLAR>({ 1.0, 0.0 });
-    constexpr auto coord_B =
-        mito::geometry::Coordinates<2, mito::geometry::POLAR>({ 1.0, 0.5 * std::numbers::pi });
+    constexpr auto coord_A = coordinates<mito::geometry::POLAR>({ 1.0, 0.0 });
+    constexpr auto coord_B = coordinates<mito::geometry::POLAR>({ 1.0, 0.5 * std::numbers::pi });
 
     // the exact value of {coord_A + coord_B} / 2.0
-    constexpr auto coord_C = mito::geometry::Coordinates<2, mito::geometry::POLAR>(
-        { std::sqrt(2.0) / 2.0, 0.25 * std::numbers::pi });
+    constexpr auto coord_C =
+        coordinates<mito::geometry::POLAR>({ std::sqrt(2.0) / 2.0, 0.25 * std::numbers::pi });
 
     // compute {coord_A + coord_B} / 2.0
     constexpr auto coord_D = (coord_A + coord_B) / 2.0;
