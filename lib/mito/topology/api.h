12d0a8a959bb5c3a34f5b5bdd775175e77ba839b
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-01-18 16:58:45 +0100

    lib/mito/topology: readd {segment_t}, {triangle_t}, {tetrahedron_t} to api

--- lib/mito/topology/api.h
+++ lib/mito/topology/api.h
@@ -12,6 +12,14 @@ namespace mito::topology {
     // vertex alias
     using vertex_t = unoriented_simplex_t<0>;
 
+    // segment alias
+    using segment_t = simplex_t<1>;
+
+    // triangle alias
+    using triangle_t = simplex_t<2>;
+
+    // tetrahedron alias
+    using tetrahedron_t = simplex_t<3>;
 }
 
 
