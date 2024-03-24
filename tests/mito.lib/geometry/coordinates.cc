aaf27ad679b8e65f4b9db5cd549294369e221d24
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-03-24 12:53:48 +0100

    tests/mito.lib/geometry: add tests for algebra in spherical coordinates

--- tests/mito.lib/geometry/coordinates.cc
+++ tests/mito.lib/geometry/coordinates.cc
@@ -50,4 +50,73 @@ TEST(Coordinates, POLAR)
 }
 
 
+TEST(Coordinates, SPHERICAL_Z)
+{
+    // create two sets of coordinates
+    constexpr auto coord_A = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 1.0, 0.5 * std::numbers::pi, 0.0 });
+    constexpr auto coord_B = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 1.0, 0.5 * std::numbers::pi, 0.5 * std::numbers::pi });
+
+    // the exact value of {coord_A + coord_B} / 2.0
+    constexpr auto coord_C = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 0.5 * std::sqrt(2.0), 0.5 * std::numbers::pi, 0.25 * std::numbers::pi });
+
+    // compute {coord_A + coord_B} / 2.0
+    constexpr auto coord_D = (coord_A + coord_B) / 2.0;
+
+    // check that the result of the average is correct
+    static_assert(coord_C == coord_D);
+
+    // check that the result of the average is correct
+    static_assert(mito::geometry::distance(coord_D, coord_C) == 0.0);
+}
+
+
+TEST(Coordinates, SPHERICAL_Y)
+{
+    // create two sets of coordinates
+    constexpr auto coord_A = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 1.0, 0.25 * std::numbers::pi, 0.0 });
+    constexpr auto coord_B = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 1.0, 0.75 * std::numbers::pi, 0.0 });
+
+    // the exact value of {coord_A + coord_B} / 2.0
+    constexpr auto coord_C = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 0.5 * std::sqrt(2.0), 0.5 * std::numbers::pi, 0.0 });
+
+    // compute {coord_A + coord_B} / 2.0
+    constexpr auto coord_D = (coord_A + coord_B) / 2.0;
+
+    // check that the result of the average is correct
+    static_assert(coord_C == coord_D);
+
+    // check that the result of the average is correct
+    static_assert(mito::geometry::distance(coord_D, coord_C) == 0.0);
+}
+
+
+TEST(Coordinates, SPHERICAL_X)
+{
+    // create two sets of coordinates
+    constexpr auto coord_A = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 1.0, 0.25 * std::numbers::pi, 0.5 * std::numbers::pi });
+    constexpr auto coord_B = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 1.0, 0.75 * std::numbers::pi, 0.5 * std::numbers::pi });
+
+    // the exact value of {coord_A + coord_B} / 2.0
+    constexpr auto coord_C = mito::geometry::coordinates<mito::geometry::SPHERICAL>(
+        { 0.5 * std::sqrt(2.0), 0.5 * std::numbers::pi, 0.5 * std::numbers::pi });
+
+    // compute {coord_A + coord_B} / 2.0
+    constexpr auto coord_D = (coord_A + coord_B) / 2.0;
+
+    // check that the result of the average is correct
+    static_assert(coord_C == coord_D);
+
+    // check that the result of the average is correct
+    static_assert(mito::geometry::distance(coord_D, coord_C) == 0.0);
+}
+
+
 // end of file
