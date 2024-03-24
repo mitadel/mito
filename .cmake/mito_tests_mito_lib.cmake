bc9a205ee91c479e4d07666a60301d338827887a
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-03-24 12:52:09 +0100

    lib/mito/manifolds: add spherical metric

--- .cmake/mito_tests_mito_lib.cmake
+++ .cmake/mito_tests_mito_lib.cmake
@@ -66,6 +66,7 @@ mito_test_driver(tests/mito.lib/manifolds/triangle_3D.cc)
 mito_test_driver(tests/mito.lib/manifolds/tetrahedron_3D.cc)
 mito_test_driver(tests/mito.lib/manifolds/tetra_rectangle_2D.cc)
 mito_test_driver(tests/mito.lib/manifolds/tetra_cube_3D.cc)
+mito_test_driver(tests/mito.lib/manifolds/volume_half_ball.cc)
 mito_test_driver(tests/mito.lib/manifolds/sphere_cartesian_coordinates.cc)
 
 # materials
--- lib/mito/manifolds/Metric.h
+++ lib/mito/manifolds/Metric.h
@@ -48,6 +48,33 @@ namespace mito::manifolds {
             return e00 + (x0 * x0) * e11;
         }
     };
+
+    // specialization for the spherical metric in 3D
+    template <>
+    struct metric<geometry::SPHERICAL, 3, 3> {
+        static constexpr auto field()
+        {
+            // the type of coordinates
+            using coordinates_t = geometry::coordinates_t<3, geometry::SPHERICAL>;
+
+            // the function extracting the x_0 component of a 3D vector
+            constexpr auto r = functions::component<coordinates_t, 0>;
+
+            // the function extracting the x_1 component of a 3D vector
+            constexpr auto t = functions::component<coordinates_t, 1>;
+
+            // the function returning the constant e_00 tensor in 3D
+            constexpr auto e_rr = fields::uniform_field<coordinates_t>(e_00<3>);
+            // the function returning the constant e_11 tensor in 3D
+            constexpr auto e_tt = fields::uniform_field<coordinates_t>(e_11<3>);
+            // the function returning the constant e_11 tensor in 3D
+            constexpr auto e_pp = fields::uniform_field<coordinates_t>(e_22<3>);
+
+            // return the metric field in spherical coordinates
+            return e_rr + mito::functions::pow<2>(r) * e_tt
+                 + mito::functions::pow<2>(r * mito::functions::sin(t)) * e_pp;
+        }
+    };
 }
 
 
