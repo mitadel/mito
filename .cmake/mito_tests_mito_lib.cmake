19035c049cccfa77cfe2cfda27cb85ea7a991a4a
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-03-24 09:34:52 +0100

    lib/mito/geometry: support for coordinate transformation CARTESIAN <-> SPHERICAL

--- .cmake/mito_tests_mito_lib.cmake
+++ .cmake/mito_tests_mito_lib.cmake
@@ -17,6 +17,7 @@ mito_test_driver(tests/mito.lib/functions/partial_derivatives.cc)
 #  geometry
 mito_test_driver(tests/mito.lib/geometry/coordinates.cc)
 mito_test_driver(tests/mito.lib/geometry/coordinate_system.cc)
+mito_test_driver(tests/mito.lib/geometry/coordinate_transformation.cc)
 mito_test_driver(tests/mito.lib/geometry/barycenter_segment_1D.cc)
 mito_test_driver(tests/mito.lib/geometry/barycenter_segment_2D.cc)
 mito_test_driver(tests/mito.lib/geometry/barycenter_segment_3D.cc)
