6d2f980089b23dc6dfb4e41495789ac8211621d5
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-04-01 11:49:42 +0200

    tests/mito.lib/math: prototype map to store function derivatives

--- tests/mito.lib/math/derivatives.cc
+++ tests/mito.lib/math/derivatives.cc
@@ -75,11 +75,17 @@ namespace mito::math {
 
     constexpr auto cos =
         ::function([](const mito::vector_t<1> & x) -> mito::scalar_t { return std::cos(x); });
+    constexpr auto sin =
+        ::function([](const mito::vector_t<1> & x) -> mito::scalar_t { return std::sin(x); });
     constexpr auto x_0 =
         ::function([](const mito::vector_t<2> & x) -> mito::scalar_t { return x[0]; });
     constexpr auto x_1 =
         ::function([](const mito::vector_t<2> & x) -> mito::scalar_t { return x[1]; });
 }
+
+
+// typedef
+using scalar_function_type = mito::scalar_t (*)(const mito::vector_t<1> &);
 
 
 int
@@ -89,6 +95,10 @@ main()
     constexpr auto f = mito::math::cos(mito::math::x_0 * mito::math::x_1);
 
     std::cout << f({ 0.0, 1.0 }) << std::endl;
+    std::map<scalar_function_type, scalar_function_type> derivatives;
+    derivatives[mito::math::sin] = mito::math::cos;
+
+    std::cout << derivatives[mito::math::sin]({ std::numbers::pi }) << std::endl;
 
     // all done
     return 0;
