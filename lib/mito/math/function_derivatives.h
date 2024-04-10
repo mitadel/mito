48d973418a95f228b8f2db55066442f8383ac4a5
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-04-10 17:39:31 +0200

    lib/mito/math/function_derivatives: derivative of negative function is now template with respect to the base function (before the negative)

--- lib/mito/math/function_derivatives.h
+++ lib/mito/math/function_derivatives.h
@@ -16,6 +16,19 @@ namespace mito::math {
     template <class F>
     constexpr auto _derivative();
 
+    // the first derivative of {f}
+    constexpr auto derivative(const Function auto & f)
+    {
+        return _derivative<std::remove_cvref_t<decltype(f)>>();
+    }
+
+    // the first derivative of the negative of a function {f}
+    template <class F>
+    constexpr auto derivative(const Negative<F> &)
+    {
+        return -_derivative<F>();
+    }
+
     template <>
     constexpr auto _derivative<Sin>()
     {
@@ -26,23 +39,6 @@ namespace mito::math {
     constexpr auto _derivative<Cos>()
     {
         return -Sin();
-    }
-
-    template <>
-    constexpr auto _derivative<Negative<Sin>>()
-    {
-        return -_derivative<Sin>();
-    }
-
-    template <>
-    constexpr auto _derivative<Negative<Cos>>()
-    {
-        return -_derivative<Cos>();
-    }
-
-    constexpr auto derivative(const Function auto & f)
-    {
-        return _derivative<std::remove_cvref_t<decltype(f)>>();
     }
 }
 
