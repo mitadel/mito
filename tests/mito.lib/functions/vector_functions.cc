24692042ebe56322e15031def4129466349cf461
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-04-13 10:42:56 +0200

    tests/mito.lib/functions: use {static_assert} when possible

--- tests/mito.lib/functions/vector_functions.cc
+++ tests/mito.lib/functions/vector_functions.cc
@@ -16,26 +16,26 @@ TEST(VectorFunctions, Components)
     // the function extracting the x_0 component
     constexpr auto x0 = mito::functions::x<0, 2>;
     // sanity check
-    EXPECT_DOUBLE_EQ(0.1, x0(x));
+    static_assert(0.1 == x0(x));
 
     // the function extracting the x_1 component
     constexpr auto x1 = mito::functions::x<1, 2>;
     // sanity check
-    EXPECT_DOUBLE_EQ(1.0, x1(x));
+    static_assert(1.0 == x1(x));
 
     // the partial derivatives of x0
     constexpr auto x0_0 = mito::functions::derivative<0>(x0);
     constexpr auto x0_1 = mito::functions::derivative<1>(x0);
     // sanity check
-    EXPECT_DOUBLE_EQ(1.0, x0_0);
-    EXPECT_DOUBLE_EQ(0.0, x0_1);
+    static_assert(1.0 == x0_0);
+    static_assert(0.0 == x0_1);
 
     // the partial derivatives of x1
     constexpr auto x1_0 = mito::functions::derivative<0>(x1);
     constexpr auto x1_1 = mito::functions::derivative<1>(x1);
     // sanity check
-    EXPECT_DOUBLE_EQ(0.0, x1_0);
-    EXPECT_DOUBLE_EQ(1.0, x1_1);
+    static_assert(0.0 == x1_0);
+    static_assert(1.0 == x1_1);
 }
 
 
@@ -50,7 +50,7 @@ TEST(VectorFunctions, Sum)
     constexpr auto x1 = mito::functions::x<1, 2>;
 
     // sanity check
-    EXPECT_DOUBLE_EQ(1.1, (x0 + x1)(x));
+    static_assert(1.1 == (x0 + x1)(x));
 }
 
 
