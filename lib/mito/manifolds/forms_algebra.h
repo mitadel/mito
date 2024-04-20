9303505f9fa28249c2347d49a51fc3b2a109b1c3
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-04-20 20:50:46 +0200

    lib/mito/manifolds: fix minor redundancy
    Implement algebra in general for P forms.

--- lib/mito/manifolds/forms_algebra.h
+++ lib/mito/manifolds/forms_algebra.h
@@ -13,7 +13,6 @@ namespace mito::manifolds {
     // addition of forms fa + fb
     template <int P, class F1, class F2>
     constexpr auto operator+(const form_t<P, F1> & fA, const form_t<P, F2> & fB)
-    requires(P > 1)
     {
         return form<P>([fA, fB]<vector_or_dummy_c... argsT>(argsT... args) {
             return fA(args...) + fB(args...);
@@ -23,24 +22,9 @@ namespace mito::manifolds {
     // scalar * form
     template <int P, class F>
     constexpr auto operator*(const real & a, const form_t<P, F> & f)
-    requires(P > 1)
     {
         return form<P>(
             [a, f]<vector_or_dummy_c... argsT>(argsT... args) { return a * f(args...); });
-    }
-
-    // addition of forms fa + fb (specialization for one-forms)
-    template <class F1, class F2>
-    constexpr auto operator+(const one_form_t<F1> & fA, const one_form_t<F2> & fB)
-    {
-        return one_form([fA, fB]<vector_or_dummy_c X>(const X & x) { return fA(x) + fB(x); });
-    }
-
-    // scalar * form (specialization for one-forms)
-    template <class F>
-    constexpr auto operator*(const real & a, const one_form_t<F> & f)
-    {
-        return one_form([a, f]<vector_or_dummy_c X>(const X & x) { return a * f(x); });
     }
 
     // form * scalar
