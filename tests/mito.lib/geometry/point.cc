57e53afa3a325c7f6a4c3cb66eece3ed09685867
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-05-19 21:02:33 +0200

    tests/mito.lib/geometry: add check that distance does not depend on the coordinate system

--- tests/mito.lib/geometry/point.cc
+++ tests/mito.lib/geometry/point.cc
@@ -10,6 +10,9 @@
 // cartesian coordinates in 2D
 using coordinates_t = mito::geometry::coordinates_t<2, mito::geometry::CARTESIAN>;
 
+// polar coordinates in 2D
+using polar_coordinates_t = mito::geometry::coordinates_t<2, mito::geometry::POLAR>;
+
 
 TEST(Point, TestPoint)
 {
@@ -19,17 +22,27 @@ TEST(Point, TestPoint)
     // the coordinate system
     auto coord_system = mito::geometry::coordinate_system<coordinates_t>();
 
-    // create and place two points
+    // create and place a point
     auto point_0 = cloud.point();
-    coord_system.place(point_0, { 0.0, 0.0 });
+    coord_system.place(point_0, { 1.0, 1.0 });
 
-    // TODO: add a test that distance does not depend on coordinate system
-    EXPECT_EQ(mito::geometry::distance(point_0, point_0, coord_system), 0.0);
+    // check the distance of a point from itself is zero
+    EXPECT_DOUBLE_EQ(mito::geometry::distance(point_0, point_0, coord_system), 0.0);
 
+    // create and place another point
     auto point_1 = cloud.point();
-    coord_system.place(point_1, { 1.0, 1.0 });
+    coord_system.place(point_1, { 2.0, 2.0 });
 
+    // check that the computed distance of the two points is correct
     EXPECT_DOUBLE_EQ(mito::geometry::distance(point_0, point_1, coord_system), std::sqrt(2.0));
+
+    // transform coordinates to a polar coordinate system
+    auto polar_coord_system = mito::geometry::coordinate_system<polar_coordinates_t>(coord_system);
+
+    // check the distances do not change
+    EXPECT_DOUBLE_EQ(mito::geometry::distance(point_0, point_0, polar_coord_system), 0.0);
+    EXPECT_DOUBLE_EQ(
+        mito::geometry::distance(point_0, point_1, polar_coord_system), std::sqrt(2.0));
 
     // print point cloud
     std::cout << cloud << std::endl;
