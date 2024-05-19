9b7d3f644cee6dea8d15a80f98e97471b20416bb
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-05-19 20:24:38 +0200

    tests/mito.lib/geometry: implement cell director test for triangle and tetrahedron too

--- tests/mito.lib/geometry/cell_directors.cc
+++ tests/mito.lib/geometry/cell_directors.cc
@@ -7,8 +7,8 @@
 #include <mito/geometry.h>
 
 
-// cartesian coordinates in 2D
-using coordinates_t = mito::geometry::coordinates_t<2, mito::geometry::CARTESIAN>;
+// cartesian coordinates in 3D
+using coordinates_t = mito::geometry::coordinates_t<3, mito::geometry::CARTESIAN>;
 
 
 TEST(Director, Segment)
@@ -17,23 +17,67 @@ TEST(Director, Segment)
     auto coord_system = mito::geometry::coordinate_system<coordinates_t>();
 
     // build two nodes
-    auto vertex_0 = mito::geometry::node(coord_system, { 0.0, 0.0 });
-    auto vertex_1 = mito::geometry::node(coord_system, { 1.0, 0.0 });
+    auto vertex_0 = mito::geometry::node(coord_system, { 0.0, 0.0, 0.0 });
+    auto vertex_1 = mito::geometry::node(coord_system, { 1.0, 0.0, 0.0 });
 
-    // build a segment embedded in 2D
-    auto segment = mito::geometry::segment<2>({ vertex_0, vertex_1 });
+    // build a segment embedded in 3D
+    auto segment = mito::geometry::segment<3>({ vertex_0, vertex_1 });
 
     // compute the cell directors
     // get the directors of the segment
     auto [_, directors] = mito::geometry::directors(segment, coord_system);
 
-    // check that the barycenter position is correct
-    EXPECT_TRUE((directors[0] == mito::vector_t<2>{ 1.0, 0.0 }));
-
-    // all done
-    return;
+    // check that the director is correct
+    EXPECT_TRUE((directors[0] == mito::vector_t<3>{ 1.0, 0.0, 0.0 }));
 }
 
-// TOFIX: implement cases for triangle and tetrahedron
+
+TEST(Director, Triangle)
+{
+    // the coordinate system
+    auto coord_system = mito::geometry::coordinate_system<coordinates_t>();
+
+    // build three nodes
+    auto vertex_0 = mito::geometry::node(coord_system, { 0.0, 0.0, 0.0 });
+    auto vertex_1 = mito::geometry::node(coord_system, { 1.0, 0.0, 0.0 });
+    auto vertex_2 = mito::geometry::node(coord_system, { 0.0, 1.0, 0.0 });
+
+    // build a triangle embedded in 3D
+    auto triangle = mito::geometry::triangle<3>({ vertex_0, vertex_1, vertex_2 });
+
+    // compute the cell directors
+    // get the directors of the triangle
+    auto [_, directors] = mito::geometry::directors(triangle, coord_system);
+
+    // check that the directors are correct
+    EXPECT_TRUE((directors[0] == mito::vector_t<3>{ 1.0, 0.0, 0.0 }));
+    EXPECT_TRUE((directors[1] == mito::vector_t<3>{ 0.0, 1.0, 0.0 }));
+}
+
+
+TEST(Director, Tetrahedron)
+{
+    // the coordinate system
+    auto coord_system = mito::geometry::coordinate_system<coordinates_t>();
+
+    // build four nodes
+    auto vertex_0 = mito::geometry::node(coord_system, { 0.0, 0.0, 0.0 });
+    auto vertex_1 = mito::geometry::node(coord_system, { 1.0, 0.0, 0.0 });
+    auto vertex_2 = mito::geometry::node(coord_system, { 0.0, 1.0, 0.0 });
+    auto vertex_3 = mito::geometry::node(coord_system, { 0.0, 0.0, 1.0 });
+
+    // build a tetrahedron embedded in 3D
+    auto tetrahedron = mito::geometry::tetrahedron<3>({ vertex_0, vertex_1, vertex_2, vertex_3 });
+
+    // compute the cell directors
+    // get the directors of the tetrahedron
+    auto [_, directors] = mito::geometry::directors(tetrahedron, coord_system);
+
+    // check that the directors are correct
+    EXPECT_TRUE((directors[0] == mito::vector_t<3>{ 1.0, 0.0, 0.0 }));
+    EXPECT_TRUE((directors[1] == mito::vector_t<3>{ 0.0, 1.0, 0.0 }));
+    EXPECT_TRUE((directors[2] == mito::vector_t<3>{ 0.0, 0.0, 1.0 }));
+}
+
 
 // end of file
