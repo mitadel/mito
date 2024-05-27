d71cc05ec0e0b3c7dc833b52903f8fd11e51c3cd
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-05-27 19:08:18 +0200

    lib/mito/io: move {MeshWriter} and {PointCloudWriter} out of {vtk} directory

--- lib/mito/io/public.h
+++ lib/mito/io/public.h
@@ -10,11 +10,24 @@
 // external packages
 #include "externals.h"
 
+// get the forward declarations
+#include "forward.h"
+
+// published type factories; this is the file you are looking for...
+#include "api.h"
+
 // classes implementation
 #include "summit/public.h"
 #ifdef WITH_VTK
 #include "vtk/public.h"
 #endif    // WITH_VTK
+
+// classes implementation
+#include "MeshWriter.h"
+#include "PointCloudWriter.h"
+
+// factories implementation
+#include "factories.h"
 
 
 // end of file
--- lib/mito/io/vtk/public.h
+++ lib/mito/io/vtk/public.h
@@ -10,22 +10,8 @@
 // external packages
 #include "externals.h"
 
-// get the forward declarations
-#include "forward.h"
-
-// published type factories; this is the file you are looking for...
-#include "api.h"
-
 // wrappers
 #include "vtk_cell.h"
 #include "vtk_point.h"
-
-// classes implementation
-#include "MeshWriter.h"
-#include "PointCloudWriter.h"
-
-// factories implementation
-#include "factories.h"
-
 
 // end of file
