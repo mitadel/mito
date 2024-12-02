fe23d6570f65f88d84b036d20dd7bf2578bb87cd
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-12-02 19:48:13 +0100

    lib/mito/materials: narrative improvements

--- lib/mito/materials/numerical.h
+++ lib/mito/materials/numerical.h
@@ -9,15 +9,17 @@
 
 namespace mito::materials {
 
-    // computes the stress tensor for material {material} at the state of deformation given by the
-    // deformation gradient {Du} as the derivative of the strain energy density with respect to the
-    // deformation gradient
+    // computes the stress tensor for material {material}, at the state of deformation given by the
+    // deformation gradient {Du}, as the numerical derivative of the strain energy density with
+    // respect to the deformation gradient. The numerical derivative is computed via centered finite
+    // differences with increment {eps}.
     auto numerical_stress(const auto & material, const matrix_c auto & Du, const real eps = 1.e-4)
         -> matrix_t<3>;
 
-    // computes the tangent tensor for material {material} at the state of deformation given by the
-    // deformation gradient {Du} as the derivative of the stress with respect to the deformation
-    // gradient
+    // computes the tangent tensor for material {material}, at the state of deformation given by the
+    // deformation gradient {Du}, as the derivative of the stress tensor with respect to the
+    // deformation gradient. The numerical derivative is computed via centered finite differences
+    // with increment {eps}.
     auto numerical_tangent(const auto & material, const matrix_c auto & Du, const real eps = 1.e-4)
         -> fourth_order_tensor_t<3>;
 
