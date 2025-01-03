5e726f1ec4baec5b77c3d36f107f56c558c836d0
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2025-01-03 09:39:28 +0100

    benchmarks: remove matrix product benchmark
    This benchmark was used for testing but it belongs to the {pyre} project.

--- .cmake/mito_benchmarks_mito_lib.cmake
+++ .cmake/mito_benchmarks_mito_lib.cmake
@@ -14,6 +14,5 @@ mito_benchmark_driver(benchmarks/mito.lib/integration/integration.cc)
 mito_benchmark_driver(benchmarks/mito.lib/fields/laplacian.cc)
 
 # tensor
-mito_benchmark_driver(benchmarks/mito.lib/tensor/matrix_times_matrix.cc)
 
 # end of file
