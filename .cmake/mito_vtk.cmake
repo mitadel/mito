f34742cc8a13e3b14cdbf79cf3aa23da9ef9559d
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2025-02-05 19:14:10 +0100

    .cmake/vtk: try to find parallel {vtk} only if {mpi} was also requested
    If the parallel components of the {vtk} library are not found, fall back on the essential components only.
    If the parallel {vtk} components are found, then the code is definition {WITH_PARALLEL_VTK} is added when building {mito} code.

--- .cmake/mito_vtk.cmake
+++ .cmake/mito_vtk.cmake
@@ -9,18 +9,45 @@ option(WITH_VTK "Enable support for VTK" OFF)
 
 # if VTK is requested
 if(WITH_VTK)
-    # the {ParallelMPI} vtk component requires the C language
-    enable_language(C)
-    # find VTK
-    find_package(VTK REQUIRED COMPONENTS CommonCore CommonDataModel IOXML IOParallelXML ParallelMPI)
-    # report
-    message(STATUS "Enable VTK support")
-    # add compiler definitions
-    add_definitions(-DWITH_VTK)
-    # include VTK headers
-    target_include_directories(mito SYSTEM PUBLIC ${VTK_INCLUDE_DIRS})
-    # link against VTK libraries
-    target_link_libraries(mito PUBLIC ${VTK_LIBRARIES})
+    # if MPI is also requested
+    if(WITH_MPI)
+        # the {ParallelMPI} vtk component requires the C language
+        enable_language(C)
+        # try to find VTK with the Parallel components too
+        find_package(VTK COMPONENTS CommonCore CommonDataModel IOXML IOParallelXML ParallelMPI)
+        # if VTK was found
+        if(VTK_FOUND)
+            # report
+            message(STATUS "Enable VTK support")
+            # add compiler definitions
+            add_definitions(-DWITH_VTK)
+            # report
+            message(STATUS "Added definition WITH_VTK")
+            # add compiler definitions
+            add_definitions(-DWITH_PARALLEL_VTK)
+            # report
+            message(STATUS "Add definition WITH_PARALLEL_VTK")
+            # include VTK headers
+            target_include_directories(mito SYSTEM PUBLIC ${VTK_INCLUDE_DIRS})
+            # link against VTK libraries
+            target_link_libraries(mito PUBLIC ${VTK_LIBRARIES})
+        endif()
+    endif()
+    # if VTK was not found yet
+    if(NOT VTK_FOUND)
+        # find the essential components of VTK
+        find_package(VTK REQUIRED COMPONENTS CommonCore CommonDataModel IOXML)
+        # report
+        message(STATUS "Enable VTK support")
+        # add compiler definitions
+        add_definitions(-DWITH_VTK)
+        # report
+        message(STATUS "Add definition WITH_VTK")
+        # include VTK headers
+        target_include_directories(mito SYSTEM PUBLIC ${VTK_INCLUDE_DIRS})
+        # link against VTK libraries
+        target_link_libraries(mito PUBLIC ${VTK_LIBRARIES})
+    endif()
 endif()
 
 
