50f5ef30cfc9bf9ccbec27488e5802a772b2346d
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2025-05-12 10:33:34 +0200

    discretization: add requirement that the geometric simplex spatial dimension matches that of the coordinate system in struct {isoparametric_simplex}

--- lib/mito/discretization/isoparametric_simplex_library.h
+++ lib/mito/discretization/isoparametric_simplex_library.h
@@ -9,13 +9,13 @@
 
 namespace mito::discretization {
 
-    // TOFIX: add requirement that the geometric simplex spatial dimension matches that of the
-    // coordinate system
     // struct storing the type of an isoparametric simplex of polynomial degree {degree} on a
     // geometric simplex of type {geometricSimplexT} and coordinate system {coordinateSystemT}
     template <
         int degree, geometry::geometric_simplex_c geometricSimplexT,
         geometry::coordinate_system_c coordinateSystemT>
+    // require that the geometric simplex spatial dimension matches that of the coordinate system
+    requires(coordinateSystemT::dim == geometricSimplexT::dim)
     struct isoparametric_simplex {};
 
     // linear shape functions on triangles
