97e11f4d628dc9c1e6278951be2f354fa2169ae4
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2025-09-06 07:47:30 +0200

    solvers: add forgotten header {solvers.h}

---
+++ lib/mito/solvers.h
@@ -0,0 +1,14 @@
+// -*- c++ -*-
+//
+// Copyright (c) 2020-2024, the MiTo Authors, all rights reserved
+//
+
+// code guard
+#pragma once
+
+
+// publish the interface
+#include "solvers/public.h"
+
+
+// end of file
