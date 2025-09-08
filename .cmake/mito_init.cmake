11c9cff64d56952a937f43eae090b21a5d0bcb75
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2025-09-08 19:30:18 +0200

    .cmake: switch to {c++23}

--- .cmake/mito_init.cmake
+++ .cmake/mito_init.cmake
@@ -30,8 +30,8 @@ function(mito_cxxInit)
     # set C++ standard
     include(CheckCXXCompilerFlag)
 
-    # request c++20
-    set(CMAKE_CXX_STANDARD 20 PARENT_SCOPE)
+    # request c++23
+    set(CMAKE_CXX_STANDARD 23 PARENT_SCOPE)
     set(CMAKE_CXX_STANDARD_REQUIRED True PARENT_SCOPE)
 
     # additional compilation flags
