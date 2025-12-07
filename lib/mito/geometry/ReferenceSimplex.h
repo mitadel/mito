d8bb6a28b2e8c3d02b1c8c527a1c863b1432e5d0
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2025-12-07 19:58:07 +0100

    geometry: add (N+1)-th coordinate to {ReferenceSimplex}
    This coordinate resolves to 1-xi0-xi1-..., consistently with what expected for barycentric coordinates.

--- lib/mito/geometry/ReferenceSimplex.h
+++ lib/mito/geometry/ReferenceSimplex.h
@@ -18,13 +18,29 @@ namespace mito::geometry {
         // the area of the reference simplex
         constexpr static double area = 1.0 / mito::tensor::factorial<order>();
 
+      private:
+        // helper to compute 1 - xi0 - xi1 - ... - xi(N-1)
+        template <int... a>
+        static constexpr auto _one_minus_xis(tensor::integer_sequence<a...>)
+        {
+            return (1.0 - ... - xi<a>);
+        }
+
       public:
         // the type of coordinates in the parametric space
         using parametric_coordinates_type = coordinates_t<N, CARTESIAN>;
 
         // the function extracting the I component of a parametric point
         template <int I>
-        static constexpr auto xi = functions::component<parametric_coordinates_type, I>;
+        static constexpr auto xi = [] {
+            if constexpr (I < N) {
+                // I-th parametric coordinate
+                return functions::component<parametric_coordinates_type, I>;
+            } else {
+                // xi<N> = 1 - xi<0> - xi<1> - ... - xi<N-1>
+                return _one_minus_xis(tensor::make_integer_sequence<N>{});
+            }
+        }();
     };
 
 }    // namespace mito
