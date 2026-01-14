cc4bedf9b9f3268c6e3595fdd81120ae4fc0e4ac
Pawel024 <p.j.rok@student.tudelft.nl>
2026-01-14 10:49:36 +0100

    tests/fem: test partition of unity and gradient consistency for the P1 segment element

--- .cmake/mito_tests_mito_lib.cmake
+++ .cmake/mito_tests_mito_lib.cmake
@@ -51,6 +51,7 @@ mito_test_driver(tests/mito.lib/fem/shape_functions_triangle_construction.cc)
 mito_test_driver(tests/mito.lib/fem/shape_functions_triangle_p1.cc)
 mito_test_driver(tests/mito.lib/fem/shape_functions_triangle_p2.cc)
 mito_test_driver(tests/mito.lib/fem/isoparametric_triangle.cc)
+mito_test_driver(tests/mito.lib/fem/isoparametric_segment.cc)
 
 # io
 mito_test_driver(tests/mito.lib/io/summit_mesh_reader_2D.cc)
