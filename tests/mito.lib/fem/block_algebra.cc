34d29960055a16da95a33516d467ca799f830ba9
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2026-08-03 15:25:15 +0200

    blocks: add test of summation of more than two blocks

--- tests/mito.lib/fem/block_algebra.cc
+++ tests/mito.lib/fem/block_algebra.cc
@@ -195,4 +195,65 @@ TEST(Fem, BlockProductSum)
     return;
 }
 
+TEST(Fem, BlockProductTwoSums)
+{
+    // the coordinate system
+    auto coord_system = mito::geometry::coordinate_system_t<coordinates_t>();
+
+    // an atlas under the coordinate system
+    auto atlas = mito::manifolds::atlas<cell_t>(coord_system);
+
+    // build nodes
+    auto node_0 = mito::geometry::node(coord_system, { 0.0, 0.0 });
+    auto node_1 = mito::geometry::node(coord_system, { 1.0, 0.0 });
+    auto node_2 = mito::geometry::node(coord_system, { 0.0, 1.0 });
+
+    // make a geometric simplex
+    auto triangle = mito::geometry::triangle(node_0, node_1, node_2);
+
+    // make a manifold element from the triangle
+    auto element = mito::manifolds::parametrized_element(
+        triangle, atlas.parametrization(triangle), metric_space_t::w);
+
+    // build the discretization nodes
+    auto discretization_node_0 = discretization_node_t();
+    auto discretization_node_1 = discretization_node_t();
+    auto discretization_node_2 = discretization_node_t();
+
+    // the degree of the finite element
+    constexpr int degree = 1;
+    // assemble the finite element type
+    using finite_element_t = mito::fem::finite_element_family<cell_t, degree>;
+
+    // a finite element
+    auto element_p1 = mito::fem::finite_element<finite_element_t>(
+        element, { discretization_node_0, discretization_node_1, discretization_node_2 });
+
+    // a mass matrix block
+    constexpr auto mass_block = mito::fem::blocks::mass_block<finite_element_t>();
+
+    // a grad-grad matric block
+    constexpr auto grad_grad_block = mito::fem::blocks::grad_grad_block<finite_element_t>();
+
+    // add them up
+    constexpr auto sum_block = mass_block + mass_block + (-1.0) * grad_grad_block;
+
+    // the analytical elementary mass matrix
+    auto analytical_block =
+        mito::tensor::matrix_t<3>{ -10.0 / 12.0, 7.0 / 12.0, 7.0 / 12.0, 7.0 / 12.0, -4.0 / 12.0,
+                                   1.0 / 12.0,   7.0 / 12.0, 1.0 / 12.0, -4.0 / 12.0 };
+
+    // compute the elementary contribution of the block
+    auto computed_block = sum_block.compute(element_p1);
+
+    // compute the error
+    auto error = mito::tensor::norm(computed_block - analytical_block);
+
+    // check the error is reasonable
+    EXPECT_NEAR(0.0, error, 1e-15);
+
+    // all done
+    return;
+}
+
 // end of file
