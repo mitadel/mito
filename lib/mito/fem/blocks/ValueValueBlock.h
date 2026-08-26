b5955e75c80c165e94c81633a187afe960000d23
Pawel024 <p.j.rok@student.tudelft.nl>
2026-08-26 13:12:25 +0200

    fem/blocks: EXPERIMENT: cache per-quadrature-point geometry in {GradientGradientBlock} and {ValueValueBlock}
    Evaluate the jacobian, pullback metric, and shape function gradients only once per quadrature point, closing the ~30-40x regression measured on {blocks.composition}
    
    Reference only: demonstrates improvements achievable with a {CellIntegrator}-level caching layer, not meant to be merged as is.

--- lib/mito/fem/blocks/ValueValueBlock.h
+++ lib/mito/fem/blocks/ValueValueBlock.h
@@ -33,40 +33,54 @@ namespace mito::fem::blocks {
         requires(element_of_type_c<elementT, element_type>)
         auto compute(const elementT & element) const -> elementary_shape
         {
-            // the parametric coordinates type
-            using parametric_coordinates_type = typename elementT::parametric_coordinates_type;
+            // CACHING EXPERIMENT: evaluate all geometric quantities exactly once per quadrature
+            // point as plain tensors
+
+            // the number of nodes per element
+            constexpr int n_nodes = element_type::n_nodes;
+            // the number of quadrature points
+            constexpr int n_quads = quadrature_rule_type::npoints;
+            // the quadrature rule
+            constexpr auto quadrature_rule = quadrature_rule_type();
+            // the measure of the reference simplex
+            constexpr auto measure = element_type::mesh_cell_type::reference_simplex_type::measure;
+
+            // the geometry of the element
+            const auto & parametrized_element = element.element();
+            const auto & phi = parametrized_element.parametrization();
+            const auto J_fn = parametrized_element.jacobian();
+            const auto & w = parametrized_element.metric_volume_form();
 
             // the elementary matrix
-            return manifolds::cell_integrator<quadrature_rule_type>(element.element())
-                .integrate(mito::functions::function([&](const parametric_coordinates_type & xi) {
-                    // the elementary contribution at quadrature point {xi}
-                    elementary_shape elementary_matrix{};
+            elementary_shape elementary_matrix{};
 
-                    // the number of nodes per element
-                    constexpr int n_nodes = element_type::n_nodes;
+            // loop on the quadrature points
+            tensor::constexpr_for_1<n_quads>([&]<int q>() {
+                constexpr auto xi = quadrature_rule.point(q);
+                constexpr auto w_q = quadrature_rule.weight(q) * measure;
 
-                    // the coordinates of the quadrature point
-                    auto x = element.parametrization()(xi);
+                // evaluate the geometry once at this quadrature point
+                const auto x = phi(xi);
+                const auto J = J_fn(xi);
+                const auto dV = w(x)(tensor::columns(J));
+                const auto coefficient = _coefficient(x);
 
-                    // evaluate the coefficient at the quadrature point
-                    auto coefficient = _coefficient(x);
+                // the shape function values at this quadrature point
+                const auto phi_values = [&]<int... a>(tensor::integer_sequence<a...>) {
+                    return std::array{ elementT::shape_functions.template shape<a>()(xi)... };
+                }(tensor::make_integer_sequence<n_nodes>{});
 
-                    // loop on the nodes of the element
-                    tensor::constexpr_for_1<n_nodes>([&]<int a>() {
-                        // evaluate the element's a-th shape function at {xi}
-                        const auto phi_a = element.template shape<a>()(xi);
-                        // loop on the nodes of the element
-                        tensor::constexpr_for_1<n_nodes>([&]<int b>() {
-                            // evaluate the element's b-th shape function at {xi}
-                            const auto phi_b = element.template shape<b>()(xi);
-                            // populate the elementary contribution to the matrix
-                            elementary_matrix[{ a, b }] = coefficient * phi_a * phi_b;
-                        });
+                // loop on the nodes of the element
+                tensor::constexpr_for_1<n_nodes>([&]<int a>() {
+                    tensor::constexpr_for_1<n_nodes>([&]<int b>() {
+                        elementary_matrix[{ a, b }] +=
+                            w_q * dV * coefficient * phi_values[a] * phi_values[b];
                     });
+                });
+            });
 
-                    // all done
-                    return elementary_matrix;
-                }));
+            // all done
+            return elementary_matrix;
         }
 
       private:
